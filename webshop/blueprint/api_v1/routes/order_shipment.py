1a542aeccdb37b275a92f05faa68daca91503766
Stan Mertens <stnmertens@gmail.com>
2023-04-10 10:46:59 +0200

    Order shipments permission bug

--- webshop/blueprint/api_v1/routes/order_shipment.py
+++ webshop/blueprint/api_v1/routes/order_shipment.py
@@ -2,30 +2,27 @@ from flask import Response
 
 from webshop.blueprint.api_v1 import api_v1_bp
 from webshop.database.client import Conn
-from webshop.database.model import Order, Shipment, OrderStatusId
-from webshop.helper.api import response, ApiText, json_get
-from webshop.helper.security import get_access
+from webshop.database.model import Order, Shipment, OrderStatusId, UserRoleLevel
+from webshop.helper.api import response, json_get
+from webshop.helper.security import authorize
 from webshop.mail.routes.order import send_order_shipped
 
 
+@authorize(UserRoleLevel.ADMIN)
 @api_v1_bp.post("/orders/<int:order_id>/shipments")
 def post_orders_id_shipments(order_id: int) -> Response:
     url, _ = json_get("url", str, nullable=False)
 
     with Conn.begin() as s:
-        # Authorize request
         # Get order
-        # Raise if order doesn't exist
-        access = get_access(s)
-        order = s.query(Order).filter_by(access_id=access.id, id=order_id).first()
-        if not order:
-            return response(403, ApiText.HTTP_403)
+        order = s.query(Order).filter_by(id=order_id).first()
 
         # Create payment
         shipment = Shipment(order_id=order_id, url=url)
         s.add(shipment)
         s.flush()
 
+        # Update order
         order.status_id = OrderStatusId.COMPLETED
         s.flush()
 
