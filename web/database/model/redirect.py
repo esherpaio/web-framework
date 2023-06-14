ff2a9989c5d7d0317971e3e787b1359e13b4cb4d
Stan Mertens <stnmertens@gmail.com>
2023-06-14 11:45:25 +0200

    Change redirect resource

--- web/database/model/redirect.py
+++ web/database/model/redirect.py
@@ -1,17 +1,10 @@
 from sqlalchemy import Column, String
 
 from . import Base
-from ._validation import check_slug
 
 
 class Redirect(Base):
     __tablename__ = "redirect"
 
-    slug_from = Column(String(256), unique=True, nullable=False)
-    slug_to = Column(String(256), nullable=False)
-
-    # Validation
-
-    @check_slug("slug_from", "slug_to")
-    def validate_slug(self, *args) -> str:
-        pass
+    url_from = Column(String(256), unique=True, nullable=False)
+    url_to = Column(String(256), nullable=False)
