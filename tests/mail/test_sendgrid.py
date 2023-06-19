da9ee51304eadfa6696efb260a2047e52f39e498
Stan Mertens <stnmertens@gmail.com>
2023-06-19 18:18:59 +0200

    Mailing improvements

--- tests/mail/test_sendgrid.py
+++ tests/mail/test_sendgrid.py
@@ -1,9 +1,9 @@
-from web.mail.base import _send_over_sengrid
+from web.mail.base import send_over_sengrid
 
 
 class TestSendGrid:
     def test_basic_email(self) -> None:
-        _send_over_sengrid(
+        send_over_sengrid(
             from_="contact@enlarge-online.nl",
             to=["contact@enlarge-online.nl"],
             subject="Web Framework unit test",
