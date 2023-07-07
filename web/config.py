f81fbeb9cb9810e7250468eacea41be61eeb1fa3
Stan Mertens <stnmertens@gmail.com>
2023-07-07 07:46:24 -0500

    Potential security issue fix

--- web/config.py
+++ web/config.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-@lru_cache
 def env_var(key: str, as_int: bool = False, as_boolean: bool = False) -> str:
     """Get an environment variable."""
     # Get value from environment
@@ -63,10 +62,10 @@ CDN_USERNAME: str = env_var("CDN_USERNAME")
 CDN_VIDEO_EXTS: list[str] = ["mp4"]
 CDN_ZONE: str = env_var("CDN_ZONE")
 
-ENDPOINT_ERROR: str | None = config_var("ENDPOINT_ERROR")
-ENDPOINT_HOME: str | None = config_var("ENDPOINT_HOME")
-ENDPOINT_LOGIN: str | None = config_var("ENDPOINT_LOGIN")
-ENDPOINT_USER: str | None = config_var("ENDPOINT_USER")
+ENDPOINT_ERROR: str = config_var("ENDPOINT_ERROR")
+ENDPOINT_HOME: str = config_var("ENDPOINT_HOME")
+ENDPOINT_LOGIN: str = config_var("ENDPOINT_LOGIN")
+ENDPOINT_USER: str = config_var("ENDPOINT_USER")
 
 DATABASE_URL: str = env_var("DATABASE_URL")
 GOOGLE_API_KEY: str = env_var("GOOGLE_API_KEY")
@@ -78,12 +77,12 @@ SEED_EXTERNAL: bool = env_var("SEED_EXTERNAL", as_boolean=True)
 ROBOT_DEFAULT_TAGS: str = config_var("ROBOT_DEFAULT_TAGS")
 ROBOT_DISALLOW_URLS: list[str] = config_var("ROBOT_DISALLOW_URLS")
 
-SOCIAL_DISCORD: str | None = config_var("SOCIAL_DISCORD")
-SOCIAL_FACEBOOK: str | None = config_var("SOCIAL_FACEBOOK")
-SOCIAL_INSTAGRAM: str | None = config_var("SOCIAL_INSTAGRAM")
-SOCIAL_PINTEREST: str | None = config_var("SOCIAL_PINTEREST")
-SOCIAL_TWITTER: str | None = config_var("SOCIAL_TWITTER")
-SOCIAL_YOUTUBE: str | None = config_var("SOCIAL_YOUTUBE")
+SOCIAL_DISCORD: str = config_var("SOCIAL_DISCORD")
+SOCIAL_FACEBOOK: str = config_var("SOCIAL_FACEBOOK")
+SOCIAL_INSTAGRAM: str = config_var("SOCIAL_INSTAGRAM")
+SOCIAL_PINTEREST: str = config_var("SOCIAL_PINTEREST")
+SOCIAL_TWITTER: str = config_var("SOCIAL_TWITTER")
+SOCIAL_YOUTUBE: str = config_var("SOCIAL_YOUTUBE")
 
 EMAIL_METHOD: str = env_var("EMAIL_METHOD")
 EMAIL_FROM: str = env_var("EMAIL_FROM")
