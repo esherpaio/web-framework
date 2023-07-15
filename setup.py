9d97253af9379b968992fe8151c58973bbbea16e
Stan Mertens <stnmertens@gmail.com>
2023-07-14 22:30:32 -0500

    Accidentally removed version

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import find_packages, setup
 
+from version import __version__
+
 
 def find_requirements() -> list[str]:
     with open("requirements.txt") as f:
@@ -9,6 +11,7 @@ def find_requirements() -> list[str]:
 setup(
     name="web-framework",
     url="https://github.com/enlarge-online/web-framework",
+    version=__version__,
     author="H.P. Mertens",
     python_requires=">=3.11",
     install_requires=find_requirements(),
