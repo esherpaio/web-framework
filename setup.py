7671c9e77f7e49afdd77e8aeb58583f0c03e7096
Stan Mertens <stnmertens@gmail.com>
2023-07-14 21:57:21 -0500

    Remove auto release

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import find_packages, setup
 
-from version import __version__
-
 
 def find_requirements() -> list[str]:
     with open("requirements.txt") as f:
@@ -9,13 +7,12 @@ def find_requirements() -> list[str]:
 
 
 setup(
+    name="web-framework",
+    url="https://github.com/enlarge-online/web-framework",
     author="H.P. Mertens",
-    include_package_data=True,
+    python_requires=">=3.11",
     install_requires=find_requirements(),
-    name="Web Framework",
+    include_package_data=True,
     package_data={"": ["mail/template/*", "i18n/translations/*"]},
     packages=find_packages(include=["web", "web.*"]),
-    python_requires=">=3.11",
-    url="https://github.com/enlarge-online/web-framework",
-    version=__version__,
 )
