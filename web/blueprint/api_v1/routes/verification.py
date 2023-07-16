a587f2e35bcba6fd5e1afdb652dad1350d54cd6a
Stan Mertens <stnmertens@gmail.com>
2023-07-15 22:15:13 -0500

    Add verification endpoint

---
+++ web/blueprint/api_v1/routes/verification.py
@@ -0,0 +1,33 @@
+from enum import StrEnum
+
+from flask import Response
+
+from web.blueprint.api_v1 import api_v1_bp
+from web.blueprint.api_v1.resource.verification import get_resource
+from web.database.client import conn
+from web.database.model import Verification
+from web.helper.api import args_get, response
+from web.i18n.base import _
+
+
+class _Text(StrEnum):
+    VERIFICATION_INVALID = _("API_VERIFICATION_INVALID")
+    VERIFICATION_KEY_NOT_FOUND = _("API_VERIFICATION_KEY_NOT_FOUND")
+
+
+@api_v1_bp.get("/verifications")
+def get_verifications() -> Response:
+    verification_key, _ = args_get("verification_key", str)
+
+    with conn.begin() as s:
+        # Get verification
+        verification = s.query(Verification).filter_by(key=verification_key).first()
+        if verification is None:
+            return response(404, _Text.VERIFICATION_KEY_NOT_FOUND)
+
+        # Check verification
+        if not verification.is_valid:
+            return response(400, _Text.VERIFICATION_INVALID)
+
+    resource = [get_resource(verification.user_id)]
+    return response(data=resource)
