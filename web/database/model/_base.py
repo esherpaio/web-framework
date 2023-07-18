863b89a3b4bdf3001af31e2ed812b6627469eb39
Stan Mertens <stnmertens@gmail.com>
2023-07-17 23:00:35 -0500

    Make base Model public

--- web/database/model/_base.py
+++ web/database/model/_base.py
@@ -2,10 +2,10 @@ from sqlalchemy import Column, DateTime, Integer, func
 from sqlalchemy.orm import declarative_base
 
 
-class _Model:
+class Model:
     id = Column(Integer, primary_key=True)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, onupdate=func.now())
 
 
-Base = declarative_base(cls=_Model)
+Base = declarative_base(cls=Model)
