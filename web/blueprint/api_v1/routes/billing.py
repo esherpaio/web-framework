c2d6d9dacca22b14e012985e12c43c700cd8c70d
Stan Mertens <stnmertens@gmail.com>
2023-07-18 21:50:15 -0500

    Class based API

--- web/blueprint/api_v1/routes/billing.py
+++ web/blueprint/api_v1/routes/billing.py
@@ -1,116 +1,82 @@
 from flask import Response
 from flask_login import current_user
+from sqlalchemy.orm.session import Session
 
 from web.blueprint.api_v1 import api_v1_bp
-from web.blueprint.api_v1.resource.billing import get_resource
-from web.database.client import conn
-from web.database.model import Billing, Cart, Order, User
-from web.helper.api import ApiText, json_get, response
+from web.blueprint.api_v1._base import API
+from web.database.model import Billing, Cart, Order
 from web.helper.cart import get_vat
+
+
+def update_carts(s: Session, billing: Billing) -> None:
+    carts = s.query(Cart).filter_by(billing_id=billing.id).all()
+    for cart in carts:
+        country_code = billing.country.code
+        is_business = billing.company is not None
+        vat_rate, vat_reverse = get_vat(country_code, is_business)
+        cart.currency_id = billing.country.currency_id
+        cart.vat_rate = vat_rate
+        cart.vat_reverse = vat_reverse
+        s.flush()
+
+
+class BillingAPI(API):
+    model = Billing
+    post_attrs = {
+        Billing.address,
+        Billing.city,
+        Billing.company,
+        Billing.country_id,
+        Billing.email,
+        Billing.first_name,
+        Billing.last_name,
+        Billing.phone,
+        Billing.vat,
+        Billing.zip_code,
+    }
+    patch_attrs = {
+        Billing.address,
+        Billing.city,
+        Billing.company,
+        Billing.country_id,
+        Billing.email,
+        Billing.first_name,
+        Billing.last_name,
+        Billing.phone,
+        Billing.vat,
+        Billing.zip_code,
+    }
+    patch_callbacks = [update_carts]
+    get_attrs = {
+        Billing.address,
+        Billing.city,
+        Billing.company,
+        Billing.country_id,
+        Billing.email,
+        Billing.first_name,
+        Billing.id,
+        Billing.last_name,
+        Billing.phone,
+        Billing.user_id,
+        Billing.vat,
+        Billing.zip_code,
+    }
 
 
 @api_v1_bp.post("/billings")
 def post_billings() -> Response:
-    address, _ = json_get("address", str, allow_empty_str=False)
-    city, _ = json_get("city", str, allow_empty_str=False)
-    company, _ = json_get("company", str, allow_empty_str=False)
-    country_id, _ = json_get("country_id", int)
-    email, _ = json_get("email", str, allow_empty_str=False)
-    first_name, _ = json_get("first_name", str, allow_empty_str=False)
-    last_name, _ = json_get("last_name", str, allow_empty_str=False)
-    phone, _ = json_get("phone", str, allow_empty_str=False)
-    vat, _ = json_get("vat", str, allow_empty_str=False)
-    zip_code, _ = json_get("zip_code", str, allow_empty_str=False)
+    api = BillingAPI()
+    return api.post(add_data={"user_id": current_user.id})
 
-    with conn.begin() as s:
-        # Insert billing
-        billing = Billing(
-            address=address,
-            city=city,
-            company=company,
-            email=email,
-            first_name=first_name,
-            last_name=last_name,
-            phone=phone,
-            vat=vat,
-            zip_code=zip_code,
-            country_id=country_id,
-        )
-        s.add(billing)
 
-    resource = get_resource(billing.id)
-    return response(data=resource)
+@api_v1_bp.get("/billings/<int:billing_id>")
+def get_billings_id(billing_id: int) -> Response:
+    api = BillingAPI()
+    return api.get(billing_id, conditions=(Billing.user_id == current_user.id,))
 
 
 @api_v1_bp.patch("/billings/<int:billing_id>")
 def patch_billings(billing_id: int) -> Response:
-    address, has_address = json_get("address", str, allow_empty_str=False)
-    city, has_city = json_get("city", str, allow_empty_str=False)
-    company, has_company = json_get("company", str, allow_empty_str=False)
-    country_id, has_country_id = json_get("country_id", int)
-    email, has_email = json_get("email", str, allow_empty_str=False)
-    first_name, has_first_name = json_get("first_name", str, allow_empty_str=False)
-    last_name, has_last_name = json_get("last_name", str, allow_empty_str=False)
-    phone, has_phone = json_get("phone", str, allow_empty_str=False)
-    vat, has_vat = json_get("vat", str, allow_empty_str=False)
-    zip_code, has_zip_code = json_get("zip_code", str, allow_empty_str=False)
-
-    with conn.begin() as s:
-        # Check if billing is in use by the user
-        cart = (
-            s.query(Cart)
-            .filter_by(user_id=current_user.id, billing_id=billing_id)
-            .first()
-        )
-        user = (
-            s.query(User).filter_by(id=current_user.id, billing_id=billing_id).first()
-        )
-        if cart is None and user is None:
-            return response(403, ApiText.HTTP_403)
-
-        # Check if billing is in use by an order
-        order = s.query(Order).filter_by(billing_id=billing_id).first()
-        if order:
-            return response(403, ApiText.HTTP_403)
-
-        # Get billing
-        billing = s.query(Billing).filter_by(id=billing_id).first()
-        if not billing:
-            return response(404, ApiText.HTTP_404)
-
-        # Update billing
-        if has_address:
-            billing.address = address
-        if has_city:
-            billing.city = city
-        if has_company:
-            billing.company = company
-        if has_email:
-            billing.email = email
-        if has_first_name:
-            billing.first_name = first_name
-        if has_last_name:
-            billing.last_name = last_name
-        if has_phone:
-            billing.phone = phone
-        if has_vat:
-            billing.vat = vat
-        if has_zip_code:
-            billing.zip_code = zip_code
-        if has_country_id:
-            billing.country_id = country_id
-        s.flush()
-
-        # Sync carts
-        carts = s.query(Cart).filter_by(billing_id=billing.id).all()
-        for cart in carts:
-            country_code = billing.country.code
-            is_business = billing.company is not None
-            vat_rate, vat_reverse = get_vat(country_code, is_business)
-            cart.currency_id = billing.country.currency_id
-            cart.vat_rate = vat_rate
-            cart.vat_reverse = vat_reverse
-            s.flush()
-
-    resource = get_resource(billing_id)
-    return response(data=resource)
+    api = BillingAPI()
+    api.raise_any_is_not_none({Order: (Order.billing_id == billing_id,)})
+    return api.patch(billing_id)
