c3461f98f0043dca68c355e0d46516f496238ee8
Stan Mertens <stnmertens@gmail.com>
2023-07-23 19:56:39 +0200

    Updated db errors

--- web/blueprint/api_v1/_bp.py
+++ web/blueprint/api_v1/_bp.py
@@ -1,33 +1,12 @@
-from enum import StrEnum
-
 from flask import Blueprint
-from werkzeug import Response
 
-from web.database.errors import DbEmailError, DbPhoneError
-from web.helper.api import response
 from web.helper.errors import handle_backend_exception
-from web.i18n.base import _
 
 api_v1_bp = Blueprint(
     name="api_v1",
     import_name=__name__,
     url_prefix="/api/v1",
 )
-
-
-class _Text(StrEnum):
-    EMAIL_ERROR = _("DATABASE_EMAIL_ERROR")
-    PHONE_ERROR = _("DATABASE_PHONE_ERROR")
-
-
-@api_v1_bp.errorhandler(DbEmailError)
-def error_email(error: DbEmailError) -> Response:
-    return response(400, _Text.EMAIL_ERROR)
-
-
-@api_v1_bp.errorhandler(DbPhoneError)
-def error_phone(error: DbPhoneError) -> Response:
-    return response(400, _Text.PHONE_ERROR)
 
 
 @api_v1_bp.errorhandler(Exception)
--- web/helper/errors.py
+++ web/helper/errors.py
@@ -1,12 +1,21 @@
+from enum import StrEnum
 from typing import Callable
 
 from flask import redirect, url_for
+from sqlalchemy.exc import IntegrityError
 from werkzeug import Response
 from werkzeug.exceptions import HTTPException
 
 from web import config
+from web.database.errors import DbEmailError, DbPhoneError
 from web.helper.api import ApiText, response
 from web.helper.logger import logger
+from web.i18n.base import _
+
+
+class _Text(StrEnum):
+    EMAIL_ERROR = _("DATABASE_EMAIL_ERROR")
+    PHONE_ERROR = _("DATABASE_PHONE_ERROR")
 
 
 def handle_frontend_exception(f: Callable) -> Callable[[Exception], Response]:
@@ -23,8 +32,18 @@ def handle_frontend_exception(f: Callable) -> Callable[[Exception], Response]:
 
 def handle_backend_exception(f: Callable) -> Callable[[Exception], Response]:
     def wrap(error: Exception) -> Response:
-        logger.error("", exc_info=True)
-        return response(500, ApiText.HTTP_500)
+        code: int
+        message: StrEnum
+        if isinstance(error, IntegrityError):
+            code, message = 409, ApiText.HTTP_409
+        elif isinstance(error, DbEmailError):
+            code, message = 400, _Text.EMAIL_ERROR
+        elif isinstance(error, DbPhoneError):
+            code, message = 400, _Text.PHONE_ERROR
+        else:
+            logger.error("", exc_info=True)
+            code, message = 500, ApiText.HTTP_500
+        return response(code, message)
 
     wrap.__name__ = f.__name__
     return wrap
