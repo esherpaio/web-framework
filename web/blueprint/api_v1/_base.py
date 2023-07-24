ceada688a557aac652761a5e7daa49f3626d1c7d
Stan Mertens <stnmertens@gmail.com>
2023-07-24 21:50:17 +0200

    API add Verification

--- web/blueprint/api_v1/_base.py
+++ web/blueprint/api_v1/_base.py
@@ -2,21 +2,25 @@ from typing import Any, Callable, Type
 
 from flask import abort, has_request_context, request
 from sqlalchemy import Column, ColumnExpressionArgument
+from sqlalchemy.orm.session import Session
 from werkzeug import Response
 
 from web.database.client import conn
 from web.database.model import Model
-from web.helper.api import ApiText, json_get, response
+from web.helper.api import ApiText, args_get, json_get, response
 
 
 class API:
     model: Type[Model] | None = None
+
     post_attrs: set[Column] = set()
     post_callbacks: list[Callable] = []
     patch_attrs: set[Column] = set()
     patch_callbacks: list[Callable] = []
+    get_args: set[Column] = set()
     get_attrs: set[Column] = set()
     get_callbacks: list[Callable] = []
+    delete_callbacks: list[Callable] = []
 
     #
     # Functions
@@ -29,11 +33,12 @@ class API:
         if not has_request_context():
             raise RuntimeError
         if request.json is None:
-            raise RuntimeError
+            return {}
 
+        request_json = request.json
         data = {}
         for attr in attrs:
-            if attr.name not in request.json:
+            if attr.name not in request_json:
                 continue
             value, _ = json_get(
                 attr.name,
@@ -44,9 +49,36 @@ class API:
             data[attr.name] = value
         return data
 
+    @classmethod
+    def _gen_query_conditions(
+        cls,
+        attrs: set[Column],
+    ) -> set[ColumnExpressionArgument[bool]]:
+        if not has_request_context():
+            raise RuntimeError
+        if request.args is None:
+            return set()
+
+        request_args = request.args.to_dict()
+        data = set()
+        for attr in attrs:
+            if attr.name not in request_args:
+                continue
+            if not hasattr(cls.model, attr.name):
+                continue
+            value, _ = args_get(
+                attr.name,
+                attr.type.python_type,
+                nullable=bool(attr.nullable),
+                default=attr.default,
+            )
+            cond = getattr(cls.model, attr.name) == value
+            data.add(cond)
+        return data
+
     @staticmethod
     def _get_query_results(
-        queries: dict[type[Model], tuple[ColumnExpressionArgument[bool], ...]],
+        queries: dict[type[Model], set[ColumnExpressionArgument[bool]]],
     ) -> list[Model | None]:
         results = []
         with conn.begin() as s:
@@ -55,6 +87,17 @@ class API:
                 results.append(result)
         return results
 
+    @classmethod
+    def _call_callbacks(
+        cls,
+        s: Session,
+        model: Type[Model] | None,
+        callbacks: list[Callable],
+    ) -> None:
+        for callback in callbacks:
+            callback(s, model)
+            s.flush()
+
     #
     # Authorization
     #
@@ -62,7 +105,7 @@ class API:
     @classmethod
     def raise_all_is_none(
         cls,
-        queries: dict[type[Model], tuple[ColumnExpressionArgument[bool], ...]],
+        queries: dict[type[Model], set[ColumnExpressionArgument[bool]]],
     ) -> None:
         """Check if all of the queries return None."""
         results = cls._get_query_results(queries)
@@ -72,7 +115,7 @@ class API:
     @classmethod
     def raise_all_is_not_none(
         cls,
-        queries: dict[type[Model], tuple[ColumnExpressionArgument[bool], ...]],
+        queries: dict[type[Model], set[ColumnExpressionArgument[bool]]],
     ) -> None:
         """Check if all of the queries return not None."""
         results = cls._get_query_results(queries)
@@ -82,7 +125,7 @@ class API:
     @classmethod
     def raise_any_is_none(
         cls,
-        queries: dict[type[Model], tuple[ColumnExpressionArgument[bool], ...]],
+        queries: dict[type[Model], set[ColumnExpressionArgument[bool]]],
     ) -> None:
         """Check if any of the queries return None."""
         results = cls._get_query_results(queries)
@@ -92,7 +135,7 @@ class API:
     @classmethod
     def raise_any_is_not_none(
         cls,
-        queries: dict[type[Model], tuple[ColumnExpressionArgument[bool], ...]],
+        queries: dict[type[Model], set[ColumnExpressionArgument[bool]]],
     ) -> None:
         """Check if any of the queries return not None."""
         results = cls._get_query_results(queries)
@@ -106,7 +149,6 @@ class API:
     @classmethod
     def post(
         cls,
-        include_request: bool = True,
         add_data: dict | None = None,
     ) -> Response:
         # Sanity checks
@@ -114,10 +156,7 @@ class API:
             raise NotImplementedError
 
         # Generate data
-        data: dict = {}
-        if include_request:
-            request_data = cls._gen_request_data(cls.post_attrs)
-            data.update(request_data)
+        data = cls._gen_request_data(cls.post_attrs)
         if add_data is not None:
             data.update(add_data)
 
@@ -125,47 +164,7 @@ class API:
         with conn.begin() as s:
             model = cls.model(**data)
             s.add(model)
-
-        return cls.get(model)
-
-    @classmethod
-    def patch(
-        cls,
-        model_id: int,
-        include_request: bool = True,
-        add_data: dict | None = None,
-        conditions: tuple[ColumnExpressionArgument[bool], ...] | None = None,
-    ) -> Response:
-        # Sanity checks
-        if cls.model is None:
-            raise NotImplementedError
-        if conditions is None:
-            conditions = ()
-
-        # Generate data
-        data: dict = {}
-        if include_request:
-            request_data = cls._gen_request_data(cls.post_attrs)
-            data.update(request_data)
-        if add_data is not None:
-            data.update(add_data)
-
-        # Patch model
-        with conn.begin() as s:
-            model = (
-                s.query(cls.model).filter(cls.model.id == model_id, *conditions).first()
-            )
-            # Check if model exists
-            if model is None:
-                abort(response(404, ApiText.HTTP_404))
-            # Update model from data
-            for k, v in data.items():
-                if hasattr(model, k):
-                    setattr(model, k, v)
-            s.flush()
-            # Run callbacks
-            for callback in cls.patch_callbacks:
-                callback(s, model)
+            cls._call_callbacks(s, model, cls.post_callbacks)
 
         return cls.get(model)
 
@@ -173,21 +172,24 @@ class API:
     def get(
         cls,
         reference: Type[Model] | int | None = None,
-        conditions: tuple[ColumnExpressionArgument[bool], ...] | None = None,
+        conditions: set[ColumnExpressionArgument[bool]] | None = None,
         as_list: bool = False,
     ) -> Response:
         # Sanity checks
         if cls.model is None:
             raise NotImplementedError
         if conditions is None:
-            conditions = ()
+            conditions = set()
+
+        # Generate query
+        query_conditions = cls._gen_query_conditions(cls.get_args)
+        conditions = conditions.union(query_conditions)
 
         # Parse reference to models
-        if reference is None:
-            with conn.begin() as s:
+        with conn.begin() as s:
+            if reference is None:
                 models = s.query(cls.model).filter(*conditions).all()
-        elif isinstance(reference, int):
-            with conn.begin() as s:
+            elif isinstance(reference, int):
                 models = [
                     (
                         s.query(cls.model)
@@ -195,8 +197,10 @@ class API:
                         .first()
                     )
                 ]
-        elif isinstance(reference, Model):
-            models = [reference]
+            elif isinstance(reference, Model):
+                models = [reference]
+            for model in models:
+                cls._call_callbacks(s, model, cls.get_callbacks)
 
         # Check if model exists
         if not models and not as_list:
@@ -217,16 +221,49 @@ class API:
             return response(data=data[0])
 
     @classmethod
-    def delete(
+    def patch(
         cls,
         model_id: int,
-        conditions: tuple[ColumnExpressionArgument[bool], ...] | None = None,
+        add_data: dict | None = None,
+        conditions: set[ColumnExpressionArgument[bool]] | None = None,
     ) -> Response:
         # Sanity checks
         if cls.model is None:
             raise NotImplementedError
         if conditions is None:
-            conditions = ()
+            conditions = set()
+
+        # Generate data
+        data = cls._gen_request_data(cls.post_attrs)
+        if add_data is not None:
+            data.update(add_data)
+
+        # Patch model
+        with conn.begin() as s:
+            model = (
+                s.query(cls.model).filter(cls.model.id == model_id, *conditions).first()
+            )
+            if model is None:
+                abort(response(404, ApiText.HTTP_404))
+            for k, v in data.items():
+                if hasattr(model, k):
+                    setattr(model, k, v)
+            s.flush()
+            cls._call_callbacks(s, model, cls.patch_callbacks)
+
+        return cls.get(model)
+
+    @classmethod
+    def delete(
+        cls,
+        model_id: int,
+        conditions: set[ColumnExpressionArgument[bool]] | None = None,
+    ) -> Response:
+        # Sanity checks
+        if cls.model is None:
+            raise NotImplementedError
+        if conditions is None:
+            conditions = set()
 
         # Delete model
         with conn.begin() as s:
@@ -236,5 +273,6 @@ class API:
             if model is None:
                 abort(response(404, ApiText.HTTP_404))
             s.delete(model)
+            cls._call_callbacks(s, model, cls.delete_callbacks)
 
         return response()
