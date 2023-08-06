21872b5ed8366ff8fa5b84f808ab18fe613192c7
Stan Mertens <stnmertens@gmail.com>
2023-08-06 17:20:05 +0200

    Updated JS

--- frontend/javascript/api/user.js
+++ frontend/javascript/api/user.js
@@ -8,27 +8,12 @@ function getUsers(data, silent = false) {
     return callApi('GET', url, data, null, silent);
 }
 
+function getUsersId(userId, silent = false) {
+    const url = `/api/v1/users/${userId}`;
+    return callApi('GET', url, null, null, silent);
+}
+
 function patchUsersId(userId, data, silent = false) {
     const url = `/api/v1/users/${userId}`;
-    return callApi('PATCH', url, data, 'application/json', silent);
-}
-
-function postUsersIdActivation(userId, silent = false) {
-    const url = `/api/v1/users/${userId}/activation`;
-    return callApi('POST', url, {}, 'application/json', silent);
-}
-
-function patchUsersIdActivation(userId, data, silent = false) {
-    const url = `/api/v1/users/${userId}/activation`;
-    return callApi('PATCH', url, data, 'application/json', silent);
-}
-
-function postUsersIdPassword(userId, silent = false) {
-    const url = `/api/v1/users/${userId}/password`;
-    return callApi('POST', url, {}, 'application/json', silent);
-}
-
-function patchUsersIdPassword(userId, data, silent = false) {
-    const url = `/api/v1/users/${userId}/password`;
     return callApi('PATCH', url, data, 'application/json', silent);
 }
