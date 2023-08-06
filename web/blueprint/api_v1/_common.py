2a6150819a307da38de73e0cc9f7b5c68defe1c8
Stan Mertens <stnmertens@gmail.com>
2023-08-06 17:30:38 +0200

    Move common functions

--- web/blueprint/api_v1/_common.py
+++ web/blueprint/api_v1/_common.py
@@ -1,19 +1,17 @@
 from mollie.api.objects.payment import Payment
 from sqlalchemy.orm import Session
 
-from web.database.model import Order, Refund
+from web.database.model import Cart, Order, Refund
 from web.document.objects.refund import gen_refund
+from web.helper.cart import get_shipment_methods
 from web.helper.fso import remove_file
 from web.helper.mollie_api import Mollie, mollie_amount
+from web.helper.objects import none_aware_attrgetter
 from web.mail.routes.order import send_order_refunded
 
 
 def create_refund(
-    s: Session,
-    mollie_payment: Payment,
-    order: Order,
-    price: float,
-    price_vat: float,
+    s: Session, mollie_payment: Payment, order: Order, price: float, price_vat: float
 ) -> None:
     # Insert refund
     refund_price = abs(price) * -1
@@ -40,3 +38,15 @@ def create_refund(
         pdf_path=pdf_path,
     )
     remove_file(pdf_path)
+
+
+def update_cart_shipment_methods(s: Session, cart: Cart) -> None:
+    shipment_methods = get_shipment_methods(s, cart)
+    if shipment_methods:
+        shipment_method = min(shipment_methods, key=none_aware_attrgetter("unit_price"))
+        cart.shipment_method_id = shipment_method.id
+        cart.shipment_price = shipment_method.unit_price * cart.currency.rate
+    else:
+        cart.shipment_method_id = None
+        cart.shipment_price = 0
+    s.flush()
