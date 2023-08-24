6f51c8451387ffbc837750d77068d29d76910d94
Stan Mertens <stnmertens@gmail.com>
2023-08-24 14:25:37 +0200

    Add cached_at setting

--- web/database/model/setting.py
+++ web/database/model/setting.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, DateTime, String
 
 from . import Base
 
@@ -7,3 +7,4 @@ class Setting(Base):
     __tablename__ = "setting"
 
     banner = Column(String(256))
+    cached_at = Column(DateTime)
