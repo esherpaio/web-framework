2fb419678b32069759a566e6c1eb8faa015352b1
Stan Mertens <stnmertens@gmail.com>
2023-08-24 14:26:16 +0200

    Updated pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,16 @@
 import base64
 import contextlib
-from typing import Generator
 
-import alembic.config
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
-from flask_login import LoginManager
-from werkzeug import Response
+from sqlalchemy.orm import Session
 
-from web import config
+from web.base import FlaskWeb
 from web.blueprint.api_v1 import api_v1_bp
 from web.blueprint.webhook_v1 import webhook_v1_bp
 from web.database.client import conn, engine
-from web.database.model import (
-    Base,
-    Country,
-    Currency,
-    Language,
-    Region,
-    User,
-    UserRoleId,
-)
-from web.helper.cache import cache
-from web.helper.user import cookie_loader, session_loader
+from web.database.model import Base, User, UserRoleId
 from web.seeder.model import (
     FileTypeSyncer,
     OrderStatusSyncer,
@@ -32,12 +19,78 @@ from web.seeder.model import (
     UserRoleSyncer,
 )
 
-# Static
-
+#
+# Pytest configuration
+#
 
 pytest_plugins = [
     "tests.fixtures.models",
 ]
+
+
+def pytest_configure(*args) -> None:
+    drop_tables()
+
+
+def drop_tables() -> None:
+    meta = Base.metadata
+    with contextlib.closing(engine.connect()) as conn:
+        trans = conn.begin()
+        for table in reversed(meta.sorted_tables):
+            conn.execute(table.delete())
+        trans.commit()
+
+
+#
+# Flask setup
+#
+
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.testing = True
+    web = FlaskWeb(
+        app,
+        blueprints=[api_v1_bp, webhook_v1_bp],
+        accept_cookie_auth=True,
+        accept_request_auth=True,
+        seed_hook=run_seeders,
+        enable_localization=True,
+    )
+    web.setup()
+    app.web = web  # type: ignore
+    return app
+
+
+def run_seeders() -> None:
+    with conn.begin() as s:
+        FileTypeSyncer().sync(s)
+        OrderStatusSyncer().sync(s)
+        ProductLinkeTypeSyncer().sync(s)
+        ProductTypeSyncer().sync(s)
+        UserRoleSyncer().sync(s)
+        create_users(s)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def update_cache(app) -> None:
+    app.web.update_cache()
+
+
+@pytest.fixture(scope="session")
+def app() -> Flask:
+    return create_app()
+
+
+@pytest.fixture(scope="session")
+def client(app) -> FlaskClient:
+    return app.test_client()
+
+
+#
+# User objects
+#
+
 user_data = [
     {
         "api_key": "guest",
@@ -57,88 +110,13 @@ user_data = [
 ]
 
 
-# Configuration
-
-
-def pytest_configure(*args) -> None:
-    alembic.config.main(argv=["upgrade", "head"])
-    drop_tables()
-    run_seeders()
-    create_users()
-
-
-def drop_tables() -> None:
-    meta = Base.metadata
-    with contextlib.closing(engine.connect()) as conn:
-        trans = conn.begin()
-        for table in reversed(meta.sorted_tables):
-            conn.execute(table.delete())
-        trans.commit()
-
-
-def run_seeders() -> None:
-    with conn.begin() as s:
-        FileTypeSyncer().sync(s)
-        OrderStatusSyncer().sync(s)
-        ProductLinkeTypeSyncer().sync(s)
-        ProductTypeSyncer().sync(s)
-        UserRoleSyncer().sync(s)
-
-
-def create_users() -> None:
-    with conn.begin() as s:
-        for data in user_data:
-            user = s.query(User).filter_by(**data).first()
-            if user is None:
-                user = User(**data)
-                s.add(user)
-                s.flush()
-
-
-def create_app() -> Flask:
-    app = Flask(__name__)
-    app.config["TESTING"] = True
-    app.config["DEBUG"] = config.APP_DEBUG
-    app.config["SECRET_KEY"] = config.APP_SECRET
-    app.register_blueprint(api_v1_bp)
-    app.register_blueprint(webhook_v1_bp)
-    login_manager = LoginManager(app)
-    login_manager.session_protection = "strong"
-    login_manager.login_view = config.ENDPOINT_LOGIN
-    login_manager.user_loader(cookie_loader)
-    login_manager.request_loader(session_loader)
-    app.add_url_rule("/login", endpoint=config.ENDPOINT_LOGIN, view_func=url_response)
-    return app
-
-
-def url_response(*args, **kwargs) -> Response:
-    return Response(status=200)
-
-
-@pytest.fixture(scope="module", autouse=True)
-def update_cache() -> None:
-    with conn.begin() as s:
-        cache.countries = s.query(Country).order_by(Country.name).all()
-        cache.currencies = s.query(Currency).order_by(Currency.code).all()
-        cache.languages = s.query(Language).order_by(Language.code).all()
-        cache.regions = s.query(Region).order_by(Region.name).all()
-
-
-# Flask app
-
-
-@pytest.fixture(scope="session")
-def app() -> Generator[Flask, None, None]:
-    app = create_app()
-    yield app
-
-
-@pytest.fixture(scope="session")
-def client(app) -> FlaskClient:
-    return app.test_client()
-
-
-# Authorization headers
+def create_users(s: Session) -> None:
+    for data in user_data:
+        user = s.query(User).filter_by(**data).first()
+        if user is None:
+            user = User(**data)
+            s.add(user)
+            s.flush()
 
 
 @pytest.fixture(scope="session")
