1704dc794be1d2477b61c7bbd5d7af50031a6116
Stan Mertens <stnmertens@gmail.com>
2024-01-01 19:44:24 -0600

    session redirect

--- web/blueprint/admin/static/js/route/session.js
+++ web/blueprint/admin/static/js/route/session.js
@@ -1,6 +1,9 @@
-async function logoutUser(redirect) {
+async function logoutUser() {
+    event.preventDefault();
     await deleteSessions();
     await updateCartCount();
+    let element = event.currentTarget;
+    let redirect = element.dataset.redirect;
     if (redirect) {
         window.location.href = redirect;
     }
