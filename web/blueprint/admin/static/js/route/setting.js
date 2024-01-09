997fbc2b3ea09bdab7064fe86e09fda91b6529ce
Stan Mertens <stnmertens@gmail.com>
2024-01-09 19:32:16 +0100

    admin panel progress

--- web/blueprint/admin/static/js/route/setting.js
+++
@@ -1,9 +0,0 @@
-async function updateSetting() {
-    event.preventDefault();
-    updateButton(`update-setting`, 1);
-    await patchSetting({
-        banner: document.getElementById('setting-banner').value,
-        cached_at: null,
-    });
-    window.location.reload();
-}
