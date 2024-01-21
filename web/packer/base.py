b80f2cc1394c51779b570edfd260c5c8d908f495
Stan Mertens <stnmertens@gmail.com>
2024-01-21 22:10:11 +0100

    rename packer to bundle

--- web/packer/base.py
+++ web/packer/base.py
@@ -12,24 +12,24 @@ class Packer:
 
     def pack(
         self,
-        packer: CssBundle | JsBundle | ScssBundle,
+        bundle: CssBundle | JsBundle | ScssBundle,
         out_dir: str | None = None,
         save_cdn: bool = True,
         *args,
         **kwargs,
     ) -> None:
-        compiled = packer.compile(*args, **kwargs)
+        compiled = bundle.compile(*args, **kwargs)
         bytes_ = compiled.encode(self.encoding)
         hash_ = hashlib.md5(bytes_).hexdigest()
         if compiled:
             if out_dir is not None:
-                out_path = os.path.join(out_dir, f"{hash_}{packer.OUT_EXT}")
+                out_path = os.path.join(out_dir, f"{hash_}{bundle.OUT_EXT}")
                 with open(out_path, "w", encoding=self.encoding) as file_:
                     file_.write(compiled)
                 logger.info(f"Saved bundle to {out_path}")
             if save_cdn:
                 file_ = io.BytesIO(bytes_)  # type: ignore
-                cdn_path = os.path.join("static", f"{hash_}{packer.OUT_EXT}")
+                cdn_path = os.path.join("static", f"{hash_}{bundle.OUT_EXT}")
                 cdn.upload(file_, cdn_path)
                 cdn_url = cdn.url(cdn_path)
                 logger.info(f"Uploaded bundle to {cdn_url}")
