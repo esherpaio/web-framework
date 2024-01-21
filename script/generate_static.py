b3c3041c9ec99f48fd57379da935d324146979af
Stan Mertens <stnmertens@gmail.com>
2024-01-21 20:22:49 +0100

    file packing

---
+++ script/generate_static.py
@@ -0,0 +1,12 @@
+from web.packer.base import CssPacker, JsPacker, Packer
+
+
+def generate_admin_static() -> None:
+    js_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/js"
+    css_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/css"
+    Packer().pack(js_dir, JsPacker(), save_cdn=True)
+    Packer().pack(css_dir, CssPacker(), save_cdn=True)
+
+
+if __name__ == "__main__":
+    generate_admin_static()
