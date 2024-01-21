feefa38c5cd65a5541f421d176322868cf6b9a1d
Stan Mertens <stnmertens@gmail.com>
2024-01-21 20:22:49 +0100

    file packing

--- web/packer/base.py
+++ web/packer/base.py
@@ -1,7 +1,9 @@
 import hashlib
+import io
 import os
 from typing import Type
 
+from web.helper import cdn
 from web.helper.logger import logger
 from web.packer.tag import CssPacker, JsPacker
 
@@ -13,7 +15,7 @@ class Packer:
         self,
         in_dir: str,
         packer: Type[CssPacker | JsPacker],
-        out_dir: str | None,
+        out_dir: str | None = None,
         save_cdn: bool = True,
     ) -> None:
         if not os.path.isdir(in_dir):
@@ -28,19 +30,25 @@ class Packer:
                 content = open(path, "r", encoding=self.encoding).read()
                 compiled += f"{packer.compile(content)}\n"
 
-        hash_ = hashlib.md5(compiled.encode(self.encoding)).hexdigest()
+        bytes_ = compiled.encode(self.encoding)
+        hash_ = hashlib.md5(bytes_).hexdigest()
         logger.info(f"Compiled {packer.OUT_EXT} with hash {hash_}")
 
-        if out_dir is not None:
-            out_path = os.path.join(out_dir, f"{hash_}.{packer.OUT_EXT}")
-            with open(out_path, "w", encoding=self.encoding) as file_:
-                file_.write(compiled)
-
-        if save_cdn:
-            pass
+        if compiled:
+            if out_dir is not None:
+                out_path = os.path.join(out_dir, f"{hash_}{packer.OUT_EXT}")
+                with open(out_path, "w", encoding=self.encoding) as file_:
+                    file_.write(compiled)
+                logger.info(f"Saved hash {hash_} to {out_path}")
+            if save_cdn:
+                file_ = io.BytesIO(bytes_)
+                cdn_path = os.path.join("static", f"{hash_}{packer.OUT_EXT}")
+                cdn.upload(file_, cdn_path)
+                logger.info(f"Uploaded hash {hash_} to {cdn_path}")
 
 
 if __name__ == "__main__":
-    in_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/js"
-    out_dir = "/Users/stan/code/web-framework"
-    packer = Packer().pack(in_dir, JsPacker(), out_dir)
+    js_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/js"
+    css_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/css"
+    Packer().pack(js_dir, JsPacker(), save_cdn=True)
+    Packer().pack(css_dir, CssPacker(), save_cdn=True)
