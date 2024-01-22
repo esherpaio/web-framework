613a736ede2ef067b2219357e30bb2df02582d22
Stan Mertens <stnmertens@gmail.com>
2024-01-22 20:53:10 +0100

    packer return value

--- web/packer/base.py
+++ web/packer/base.py
@@ -17,7 +17,8 @@ class Packer:
         save_cdn: bool = True,
         *args,
         **kwargs,
-    ) -> None:
+    ) -> tuple[str | None, str | None]:
+        out_path, cdn_url = None, None
         compiled = bundle.compile(*args, **kwargs)
         bytes_ = compiled.encode(self.encoding)
         hash_ = hashlib.md5(bytes_).hexdigest()
@@ -33,3 +34,4 @@ class Packer:
                 cdn.upload(file_, cdn_path)
                 cdn_url = cdn.url(cdn_path)
                 logger.info(f"Uploaded bundle to {cdn_url}")
+        return out_path, cdn_url
