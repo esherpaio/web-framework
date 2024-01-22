5f0580161ee4653d360aba022b3a930a74ff4813
Stan Mertens <stnmertens@gmail.com>
2024-01-22 20:44:35 +0100

    changes

--- web/database/model/flask_route.py
+++ web/database/model/flask_route.py
@@ -9,10 +9,11 @@ class FlaskRoute(Base):
     __tablename__ = "flask_route"
 
     attributes = MC(type_json, nullable=False, server_default="{}")
+    css_path = MC(String(128))
     description = MC(String(256))
     endpoint = MC(String(64), unique=True, nullable=False)
     image_url = MC(String(256))
     in_sitemap = MC(Boolean, nullable=False)
+    js_path = MC(String(128))
     name = MC(String(64), nullable=False)
     robots = MC(String(256), nullable=False)
-    static_path = MC(String(128))
