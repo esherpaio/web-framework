940de8006fefde82b35aedc45f292d787cdd1028
Stan Mertens <stnmertens@gmail.com>
2024-01-28 10:52:17 +0100

    various improvements

--- web/helper/builtins.py
+++ web/helper/builtins.py
@@ -2,6 +2,20 @@ from operator import attrgetter
 from typing import Any, Callable
 
 #
+# Classes
+#
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+#
 # Functions
 #
 
