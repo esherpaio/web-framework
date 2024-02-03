9396f99bca02ee59a0ca7126e91792a05df23040
Stan Mertens <stnmertens@gmail.com>
2024-02-03 19:11:27 +0100

    email update

--- web/mail/routes/order.py
+++ web/mail/routes/order.py
@@ -7,17 +7,19 @@ def send_order_received(
     order_id: int,
     billing_email: str,
     shipping_email: str,
+    **kwargs,
 ) -> None:
     to = [billing_email, shipping_email]
     subject = _(
         "MAIL_ORDER_SUBJECT", business_name=config.BUSINESS_NAME, order_id=order_id
     )
-    title = _("MAIL_ORDER_TITLE", order_id=order_id)
-    paragraphs = [
-        _("MAIL_ORDER_RECEIVED_P1", business_name=config.BUSINESS_NAME),
-        _("MAIL_ORDER_RECEIVED_P2"),
-    ]
-    html = render_email(title, paragraphs)
+    html = render_email(
+        title=_("MAIL_ORDER_TITLE", order_id=order_id),
+        paragraphs=[
+            _("MAIL_ORDER_RECEIVED_P1", business_name=config.BUSINESS_NAME),
+            _("MAIL_ORDER_RECEIVED_P2"),
+        ],
+    )
     send_email(to, subject, html)
 
 
@@ -26,15 +28,17 @@ def send_order_paid(
     billing_email: str,
     invoice_number: str,
     pdf_path: str,
+    **kwargs,
 ) -> None:
     to = [billing_email]
     subject = _(
         "MAIL_ORDER_SUBJECT", business_name=config.BUSINESS_NAME, order_id=order_id
     )
-    title = _("MAIL_ORDER_TITLE", order_id=order_id)
-    paragraphs = [_("MAIL_ORDER_PAID_P1"), _("MAIL_ORDER_PAID_P2")]
+    html = render_email(
+        title=_("MAIL_ORDER_TITLE", order_id=order_id),
+        paragraphs=[_("MAIL_ORDER_PAID_P1"), _("MAIL_ORDER_PAID_P2")],
+    )
     pdf_name = _("MAIL_ORDER_PAID_FILENAME", invoice_number=invoice_number)
-    html = render_email(title, paragraphs)
     send_email(to, subject, html, blob_path=pdf_path, blob_name=pdf_name)
 
 
@@ -44,18 +48,20 @@ def send_order_shipped(
     billing_email: str,
     shipping_email: str,
     shipping_address: str,
+    **kwargs,
 ) -> None:
     to = [billing_email, shipping_email]
     subject = _(
         "MAIL_ORDER_SUBJECT", business_name=config.BUSINESS_NAME, order_id=order_id
     )
-    title = _("MAIL_ORDER_TITLE", order_id=order_id)
-    paragraphs = [
-        _("MAIL_ORDER_SHIPPED_P1"),
-        _("MAIL_ORDER_SHIPPED_P2", shipment_url=shipment_url),
-        _("MAIL_ORDER_SHIPPED_P3", shipping_address=shipping_address),
-    ]
-    html = render_email(title, paragraphs)
+    html = render_email(
+        title=_("MAIL_ORDER_TITLE", order_id=order_id),
+        paragraphs=[
+            _("MAIL_ORDER_SHIPPED_P1"),
+            _("MAIL_ORDER_SHIPPED_P2", shipment_url=shipment_url),
+            _("MAIL_ORDER_SHIPPED_P3", shipping_address=shipping_address),
+        ],
+    )
     send_email(to, subject, html)
 
 
@@ -64,13 +70,15 @@ def send_order_refunded(
     billing_email: str,
     refund_number: int,
     pdf_path: str,
+    **kwargs,
 ) -> None:
     to = [billing_email]
     subject = _(
         "MAIL_ORDER_SUBJECT", business_name=config.BUSINESS_NAME, order_id=order_id
     )
-    title = _("MAIL_ORDER_TITLE", order_id=order_id)
-    paragraphs = [_("MAIL_ORDER_REFUNDED_P1"), _("MAIL_ORDER_REFUNDED_P2")]
+    html = render_email(
+        title=_("MAIL_ORDER_TITLE", order_id=order_id),
+        paragraphs=[_("MAIL_ORDER_REFUNDED_P1"), _("MAIL_ORDER_REFUNDED_P2")],
+    )
     pdf_name = _("MAIL_ORDER_REFUNDED_FILENAME", refund_number=refund_number)
-    html = render_email(title, paragraphs)
     send_email(to, subject, html, blob_path=pdf_path, blob_name=pdf_name)
