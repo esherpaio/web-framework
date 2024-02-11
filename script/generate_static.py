5f4eb8639c932d2362e94e3640a145314e8f00a3
Stan Mertens <stnmertens@gmail.com>
2024-02-11 14:52:15 +0100

    various fixes

--- script/generate_static.py
+++ script/generate_static.py
@@ -2,8 +2,8 @@ from web.packer.base import CssBundle, JsBundle, Packer
 
 
 def generate_admin_static() -> None:
-    js_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/js"
-    css_dir = "/Users/stan/code/web-framework/web/blueprint/admin/static/css"
+    js_dir = "/home/stan/code/web-framework/web/blueprint/admin/static/js"
+    css_dir = "/home/stan/code/web-framework/web/blueprint/admin/static/css"
     Packer().pack(JsBundle(js_dir), save_cdn=True)
     Packer().pack(CssBundle(css_dir), save_cdn=True)
 
