3f569b8ad19b554f99c16d0f62687180a3daacd9
Stan Mertens <stnmertens@gmail.com>
2024-03-03 12:07:41 +0100

    remove offline migrations

--- sandbox/migrate/env.py
+++ sandbox/migrate/env.py
@@ -1,23 +1,7 @@
 from alembic import context
 
-from web.config import config
-from web.database.client import engine
+from web.database import engine
 from web.database.model import Base
-
-
-def run_migrations_offline() -> None:
-    """Run migrations in 'offline' mode."""
-
-    context.config.set_main_option("sqlalchemy.url", config.DATABASE_URL)
-    context.configure(
-        url=config.DATABASE_URL,
-        target_metadata=Base.metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-        compare_server_default=True,
-    )
-    with context.begin_transaction():
-        context.run_migrations()
 
 
 def run_migrations_online() -> None:
@@ -34,7 +18,4 @@ def run_migrations_online() -> None:
             context.run_migrations()
 
 
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+run_migrations_online()
