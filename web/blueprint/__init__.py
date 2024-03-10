38452c94675ada7f4324bba905c560410479ba2a
Stan Mertens <stnmertens@gmail.com>
2024-03-10 17:32:51 +0100

    bug fix

--- web/blueprint/__init__.py
+++ web/blueprint/__init__.py
@@ -1,9 +1,11 @@
 from .admin._bp import admin_bp
 from .api_v1._bp import api_v1_bp
+from .robots._bp import robots_bp
 from .webhook_v1._bp import webhook_v1_bp
 
 __all__ = [
     "admin_bp",
     "api_v1_bp",
+    "robots_bp",
     "webhook_v1_bp",
 ]
