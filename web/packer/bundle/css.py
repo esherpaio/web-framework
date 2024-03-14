9d7b6e8a7e2c64c87322f5055dd8b2053d20da10
Stan Mertens <stnmertens@gmail.com>
2024-03-14 21:45:38 +0100

    tryout rcssmin

--- web/packer/bundle/css.py
+++ web/packer/bundle/css.py
@@ -1,5 +1,7 @@
 import os
-import re
+
+# import re
+import rcssmin
 
 
 class CssBundle:
@@ -26,16 +28,17 @@ class CssBundle:
 
     @staticmethod
     def _compile(css: str) -> str:
-        # remove comments
-        css = re.sub(r"\s*/\*\s*\*/", "$$HACK1$$", css)  # preserve IE<6 comment hack
-        css = re.sub(r"/\*[\s\S]*?\*/", "", css)
-        css = css.replace("$$HACK1$$", "/**/")  # preserve IE<6 comment hack
-        # spaces may be safely collapsed as generated content will collapse them anyway
-        css = re.sub(r"\s+", " ", css)
-        # shorten collapsable colors: #aabbcc to #abc
-        css = re.sub(r"#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3(\s|;)", r"#\1\2\3\4", css)
-        # fragment values can loose zeros
-        css = re.sub(r":\s*0(\.\d+([cm]m|e[mx]|in|p[ctx]))\s*;", r":\1;", css)
-        # strip css
-        css = css.strip()
+        # # remove comments
+        # css = re.sub(r"\s*/\*\s*\*/", "$$HACK1$$", css)  # preserve IE<6 comment hack
+        # css = re.sub(r"/\*[\s\S]*?\*/", "", css)
+        # css = css.replace("$$HACK1$$", "/**/")  # preserve IE<6 comment hack
+        # # spaces may be safely collapsed as generated content will collapse them anyway
+        # css = re.sub(r"\s+", " ", css)
+        # # shorten collapsable colors: #aabbcc to #abc
+        # css = re.sub(r"#([0-9a-f])\1([0-9a-f])\2([0-9a-f])\3(\s|;)", r"#\1\2\3\4", css)
+        # # fragment values can loose zeros
+        # css = re.sub(r":\s*0(\.\d+([cm]m|e[mx]|in|p[ctx]))\s*;", r":\1;", css)
+        # # strip css
+        # css = css.strip()
+        rcssmin.cssmin(css, keep_bang_comments=False)
         return css
