8bc35dee1c761dc49e8bd2a12a228147f13cd3e1
Stan Mertens <stnmertens@gmail.com>
2024-03-24 16:28:30 +0100

    order table update part 1

---
+++ sandbox/migrate/version/20240324_162637.py
@@ -0,0 +1,25 @@
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import orm
+
+from web.database.model import Order
+
+revision = "423ebcd85503"
+down_revision = "9915034938ed"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Get session
+    bind = op.get_bind()
+    s = orm.Session(bind=bind)
+    # Add currency_code column
+    op.add_column("order", sa.Column("currency_code", sa.String(length=3)))
+    # Set currency_code on all orders
+    for order in s.query(Order).join(Order.currency).all():
+        order.currency_code = order.currency.code
+    # Make curency_code not nullable
+    op.alter_column(
+        "order", "currency_code", existing_type=sa.String(length=3), nullable=False
+    )
