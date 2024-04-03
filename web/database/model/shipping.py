1d7746e66889d3cf71feb18026dd3acdea58c397
Stan Mertens <stnmertens@gmail.com>
2024-04-03 19:56:39 +0200

    improved parsed text

--- web/database/model/shipping.py
+++ web/database/model/shipping.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import mapped_column as MC
 from sqlalchemy.orm import relationship, validates
 
 from ._base import Base
-from ._utils import del_emoji, get_lower, val_email, val_length, val_phone
+from ._utils import get_lower, parse_text, val_email, val_length, val_phone
 
 
 class Shipping(Base):
@@ -35,8 +35,8 @@ class Shipping(Base):
         "address", "city", "state", "zip_code", "company", "first_name", "last_name"
     )
     def validate_address(self, key: str, value: Any) -> Any:
+        value = parse_text(value)
         val_length(value, min_=2)
-        value = del_emoji(value)
         return value
 
     @validates("email")
