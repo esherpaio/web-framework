198057c8d21e691c527c437de4249a3193cf7948
Stan Mertens <stnmertens@gmail.com>
2024-08-03 20:17:23 +0200

    remove all blueprint error handlers

--- web/blueprint/api_v1/_bp.py
+++ web/blueprint/api_v1/_bp.py
@@ -1,10 +1,7 @@
 from flask import Blueprint
 
-from web.libs.app import handle_backend_error
-
 api_v1_bp = Blueprint(
     name="api_v1",
     import_name=__name__,
     url_prefix="/api/v1",
 )
-api_v1_bp.register_error_handler(Exception, handle_backend_error)
