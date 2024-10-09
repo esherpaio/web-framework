8b20e68d9b1b86aad473fa878c9eba4f6f40d9e5
Stan Mertens <stnmertens@gmail.com>
2024-10-09 14:00:21 +0700

    gtags progress

--- web/app/gtag.py
+++ web/app/gtag.py
@@ -3,34 +3,57 @@ from enum import StrEnum
 
 from markupsafe import Markup
 
+#
+# Events
+#
 
-class EventType(StrEnum):
+
+class EventTrigger(StrEnum):
     ON_LOAD = "load"
     ON_CLICK = "click"
 
 
-class EventName(StrEnum):
-    SIGN_UP = "sign_up"
-    LOGIN = "login"
-    VIEW_ITEM_LIST = "view_item_list"
-    VIEW_ITEM = "view_item"
-    ADD_TO_CART = "add_to_cart"
-    VIEW_CART = "view_cart"
-    REMOVE_FROM_CART = "remove_from_cart"
-    BEGIN_CHECKOUT = "begin_checkout"
-    ADD_SHIPPING_INFO = "add_shipping_info"
-    ADD_PAYMENT_INFO = "add_payment_info"
-    PURCHASE = "purchase"
+class Event:
+    def js_listener(self, *args, **kwargs) -> str:
+        raise NotImplementedError
+
+    def js_function(self, name: str, data: dict | None = None) -> str:
+        if data is None:
+            data = {}
+        return f"function (e) {{ gtag('event', '{name}', {json.dumps(data)}); }}"
 
 
-class GTagEvent:
-    """A class to generate Google tags code."""
+class EventByWindow(Event):
+    def __init__(self, trigger: EventTrigger) -> None:
+        self.trigger = trigger
 
+    def js_listener(self, name: str, data: dict | None = None) -> str:
+        function = self.js_function(name, data)
+        return Markup(f"window.addEventListener('{self.trigger}', {function});")
+
+
+class EventById(Event):
+    def __init__(self, element_id: str, trigger: EventTrigger) -> None:
+        self.id_ = element_id
+        self.trigger = trigger
+
+    def js_listener(self, name: str, data: dict | None = None) -> str:
+        function = self.js_function(name, data)
+        return Markup(
+            f"document.getElementById('{self.id_}').addEventListener('{self.trigger}', {function});"
+        )
+
+
+#
+# Google tags
+#
+
+
+class Gtag:
     NAME: str
 
-    def __init__(self, id_: str, type_: EventType) -> None:
-        self.id = id_
-        self.type = type_
+    def __init__(self, by: Event) -> None:
+        self.by = by
         self._data: dict = {}
 
     @property
@@ -46,66 +69,59 @@ class GTagEvent:
 
     @property
     def code(self) -> Markup:
-        return Markup(self._js_listener)
-
-    @property
-    def _js_listener(self) -> Markup:
-        return Markup(
-            f"document.getElementById('{self.id}').addEventListener('{self.type}', {self.js_push_func});"
-        )
-
-    @property
-    def js_push_func(self) -> Markup:
-        return Markup(
-            f"function (e) {{ gtag('event', '{self.NAME}', {json.dumps(self.data)}); }}"
-        )
+        return Markup(self.by.js_listener(self.NAME, self.data))
 
 
-class GTagSignUp(GTagEvent):
+class GtagSignUp(Gtag):
     NAME = "sign_up"
 
 
-class GTagLogin(GTagEvent):
+class GtagLogin(Gtag):
     NAME = "login"
 
 
-class GTagViewItemList(GTagEvent):
+class GtagViewItemList(Gtag):
     NAME = "view_item_list"
 
 
-class GTagViewItem(GTagEvent):
+class GtagViewItem(Gtag):
     NAME = "view_item"
 
 
-class GTagAddToCart(GTagEvent):
+class GtagAddToCart(Gtag):
     NAME = "add_to_cart"
 
 
-class GTagViewCart(GTagEvent):
+class GtagViewCart(Gtag):
     NAME = "view_cart"
 
 
-class GTagRemoveFromCart(GTagEvent):
+class GtagRemoveFromCart(Gtag):
     NAME = "remove_from_cart"
 
 
-class GTagBeginCheckout(GTagEvent):
+class GtagBeginCheckout(Gtag):
     NAME = "begin_checkout"
 
 
-class GTagAddShippingInfo(GTagEvent):
+class GtagAddShippingInfo(Gtag):
     NAME = "add_shipping_info"
 
 
-class GTagAddPaymentInfo(GTagEvent):
+class GtagAddPaymentInfo(Gtag):
     NAME = "add_payment_info"
 
 
-class GTagPurchase(GTagEvent):
+class GtagPurchase(Gtag):
     NAME = "purchase"
 
 
-def gen_gtags(gtags: list[GTagEvent] | None = None) -> list[GTagEvent]:
+#
+# Utilities
+#
+
+
+def gen_gtags(gtags: list[Gtag] | None = None) -> list[Gtag]:
     if gtags is None:
         gtags = []
     return gtags
