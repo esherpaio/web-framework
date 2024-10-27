64ef1c44d04be6635bdd26969e763df952f2185a
Stan Mertens <stnmertens@gmail.com>
2024-10-27 16:48:39 +0100

    allow extra paths in scss packer

--- web/packer/bundle/scss.py
+++ web/packer/bundle/scss.py
@@ -6,9 +6,16 @@ from .css import CssBundle
 class ScssBundle:
     OUT_EXT = ".css"
 
-    def __init__(self, in_path: str) -> None:
+    def __init__(self, in_path: str, extra_paths: list[str] | None = None) -> None:
         self._in_path = in_path
+        if extra_paths is None:
+            extra_paths = []
+        self._extra_paths = extra_paths
 
     def compile(self) -> str:
-        scss = sass.compile(filename=self._in_path, output_style="compressed")
+        scss = sass.compile(
+            filename=self._in_path,
+            output_style="compressed",
+            include_paths=self._extra_paths,
+        )
         return CssBundle._compile(scss)
