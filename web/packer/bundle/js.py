a9c3bc0e9ff4b672e2caade135e9e25251e40fb1
Stan Mertens <stnmertens@gmail.com>
2024-10-27 16:55:30 +0100

    multiple js dirs

--- web/packer/bundle/js.py
+++ web/packer/bundle/js.py
@@ -8,20 +8,21 @@ class JsBundle(object):
     IN_EXTS = [".js"]
     OUT_EXT = ".js"
 
-    def __init__(self, in_dir: str, follow_subdirs: bool = True) -> None:
-        self._in_dir = in_dir
+    def __init__(self, in_dirs: list[str], follow_subdirs: bool = True) -> None:
+        self._in_dirs = in_dirs
         self._follow_subdirs = follow_subdirs
 
     def compile(self) -> str:
         compiled = ""
-        for subdir, _, fns in os.walk(self._in_dir):
-            for fn in fns:
-                if not any([fn.endswith(ext) for ext in self.IN_EXTS]):
-                    continue
-                path = os.path.join(subdir, fn)
-                content = open(path, "r", encoding=self.ENCODING).read()
-                compiled += jsmin(content, keep_bang_comments=False)
-                compiled += "\n"
-            if not self._follow_subdirs:
-                break
+        for in_dir in self._in_dirs:
+            for subdir, _, fns in os.walk(in_dir):
+                for fn in fns:
+                    if not any([fn.endswith(ext) for ext in self.IN_EXTS]):
+                        continue
+                    path = os.path.join(subdir, fn)
+                    content = open(path, "r", encoding=self.ENCODING).read()
+                    compiled += jsmin(content, keep_bang_comments=False)
+                    compiled += "\n"
+                if not self._follow_subdirs:
+                    break
         return compiled
