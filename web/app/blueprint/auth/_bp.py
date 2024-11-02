a52082c3026224eca519c95ac5f3c4a6444d718b
Stan Mertens <stnmertens@gmail.com>
2024-11-02 23:01:44 +0100

    add auth blueprint

---
+++ web/app/blueprint/auth/_bp.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Blueprint
+from web.app.meta import gen_meta
+from web.app.routing import get_route
+from web.app.schema import gen_schemas
+
+_dir = os.path.dirname(os.path.abspath(__file__))
+auth_bp = Blueprint(
+    name="auth",
+    import_name=__name__,
+    url_prefix=None,
+    template_folder=os.path.join(_dir, "templates"),
+    static_folder=os.path.join(_dir, "static"),
+    static_url_path="/auth/static",
+)
+
+
+@auth_bp.context_processor
+def context() -> dict:
+    route = get_route()
+    meta = gen_meta(route)
+    schemas = gen_schemas(route)
+    return dict(meta=meta, schemas=schemas)
---
+++ web/app/blueprint/auth/static/js/login.js
@@ -0,0 +1,48 @@
+async function loginUser() {
+    event.preventDefault();
+    const buttonId = 'button-login-user';
+    updateButton(buttonId, 1);
+    await postSessions({
+        email: document.getElementById('login-email').value,
+        password: document.getElementById('login-password').value,
+        remember: true,
+    });
+    await updateCartCount();
+    window.location.href = URL_USER_SETTINGS;
+}
+
+async function loginUserGoogle(resp) {
+    const buttonId = 'button-login-user';
+    updateButton(buttonId, 1);
+    if (resp && resp.credential) {
+        await postSessionsGoogle({ token_id: resp.credential });
+        window.location.href = URL_USER_SETTINGS;
+    }
+    updateButton(buttonId, -1);
+}
+
+async function verifyUser(verificationKey) {
+    let resp = await getVerifications({ key: verificationKey });
+    if (resp && resp.data.length > 0) {
+        let verification = resp.data[0];
+        resp = await patchUsersIdActivation(verification.user_id, { verification_key: verificationKey });
+        showMessage(resp.message);
+    }
+}
+
+async function checkLoginParams() {
+    const params = new URLSearchParams(window.location.search);
+    const matchParam = 'verification_key';
+    const match = params.has(matchParam);
+    if (match) {
+        const verificationKey = getParameter(matchParam);
+        removeParameter(matchParam);
+        await verifyUser(verificationKey);
+    }
+}
+
+window.addEventListener("load", () => {
+    if (isLoad('auth-login')) {
+        checkLoginParams();
+    };
+});
