acad292a7f307a24f5ce1373e66b73db6e59e761
Stan Mertens <stnmertens@gmail.com>
2024-11-02 22:57:55 +0100

    refactor; update packer;

--- web/packer/bundle/scss.py
+++ web/packer/bundle/scss.py
@@ -6,16 +6,14 @@ from .css import CssBundle
 class ScssBundle:
     OUT_EXT = ".css"
 
-    def __init__(self, in_path: str, extra_paths: list[str] | None = None) -> None:
+    def __init__(self, in_path: str, build_paths: str | None = None) -> None:
         self._in_path = in_path
-        if extra_paths is None:
-            extra_paths = []
-        self._extra_paths = extra_paths
+        self._build_paths = [] if build_paths is None else [build_paths]
 
     def compile(self) -> str:
         scss = sass.compile(
             filename=self._in_path,
             output_style="compressed",
-            include_paths=self._extra_paths,
+            include_paths=self._build_paths,
         )
         return CssBundle._compile(scss)
