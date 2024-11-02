9734178142e5b29c4a1ebf2890b26570e775e708
Stan Mertens <stnmertens@gmail.com>
2024-11-02 22:57:55 +0100

    refactor; update packer;

--- web/packer/packer.py
+++ web/packer/packer.py
@@ -25,14 +25,22 @@ class Packer:
         hash_ = hashlib.md5(bytes_).hexdigest()
         if compiled:
             if out_dir is not None:
-                out_path = os.path.join(out_dir, f"{hash_}{bundle.OUT_EXT}")
-                with open(out_path, "w", encoding=self.ENCODING) as file_:
-                    file_.write(compiled)
-                log.info(f"Saved bundle to {out_path}")
+                out_path = self.write_file(compiled, hash_, out_dir, bundle.OUT_EXT)
             if save_cdn:
-                fileb = io.BytesIO(bytes_)
-                cdn_path = os.path.join("static", f"{hash_}{bundle.OUT_EXT}")
-                cdn.upload(fileb, cdn_path)
-                cdn_url = cdn.url(cdn_path)
-                log.info(f"Uploaded bundle to {cdn_url}")
+                cdn_path = self.write_cdn(bytes_, hash_, bundle.OUT_EXT)
         return out_path, cdn_path
+
+    def write_file(self, data: str, hash_: str, out_dir: str, out_ext: str) -> str:
+        out_path = os.path.join(out_dir, f"{hash_}{out_ext}")
+        with open(out_path, "w", encoding=self.ENCODING) as file_:
+            file_.write(data)
+        log.info(f"Saved bundle to {out_path}")
+        return out_path
+
+    def write_cdn(self, data: bytes, hash_: str, out_ext: str) -> str:
+        cdn_path = os.path.join("static", f"{hash_}{out_ext}")
+        fileb = io.BytesIO(data)
+        cdn.upload(fileb, cdn_path)
+        cdn_url = cdn.url(cdn_path)
+        log.info(f"Uploaded bundle to {cdn_url}")
+        return cdn_path
