66aadac7db04cfed52a339b5b369575ad639803e
Stan Mertens <stnmertens@gmail.com>
2024-11-02 23:17:48 +0100

    fixes for auth blueprint;

--- web/blueprint/__init__.py
+++ web/blueprint/__init__.py
@@ -1,11 +1,13 @@
 from .admin._bp import admin_bp
 from .api_v1._bp import api_v1_bp
+from .auth import auth_bp
 from .robots._bp import robots_bp
 from .webhook_v1._bp import webhook_v1_bp
 
 __all__ = [
     "admin_bp",
     "api_v1_bp",
+    "auth_bp",
     "robots_bp",
     "webhook_v1_bp",
 ]
