3ddb68f13b2c1fcfd288a358c43d90b7288d7a16
Stan Mertens <stnmertens@gmail.com>
2024-11-02 23:10:03 +0100

    fixes

---
+++ web/blueprint/api_v1/routes/product_value.py
@@ -0,0 +1,109 @@
+from sqlalchemy import false
+from sqlalchemy.orm import contains_eager
+from werkzeug import Response
+
+from web.api import ApiText, json_get, json_response
+from web.auth import authorize
+from web.automation import sync_after
+from web.automation.task import SkuSyncer
+from web.blueprint.api_v1 import api_v1_bp
+from web.database import conn
+from web.database.model import ProductValue, Sku, SkuDetail, UserRoleLevel
+from web.validation import gen_slug
+
+#
+# Configuration
+#
+
+
+#
+# Endpoints
+#
+
+
+@api_v1_bp.post("/products/<int:product_id>/values")
+@authorize(UserRoleLevel.ADMIN)
+def post_products_id_values(product_id: int) -> Response:
+    name, _ = json_get("name", str, nullable=False)
+    option_id, _ = json_get("option_id", int, nullable=False)
+    order, _ = json_get("order", int)
+    unit_price, _ = json_get("unit_price", int | float, nullable=True)
+
+    with conn.begin() as s:
+        # Get or restore product value
+        product_value = (
+            s.query(ProductValue)
+            .filter_by(option_id=option_id, slug=gen_slug(name))
+            .first()
+        )
+        if product_value:
+            if product_value.is_deleted:
+                product_value.is_deleted = False
+                return json_response()
+            else:
+                return json_response(409, ApiText.HTTP_409)
+
+        # Insert product value
+        product_value = ProductValue(
+            option_id=option_id,
+            name=name,
+            unit_price=unit_price,
+            order=order,
+        )
+        s.add(product_value)
+
+    return json_response()
+
+
+@api_v1_bp.patch("/products/<int:product_id>/values/<int:value_id>")
+@authorize(UserRoleLevel.ADMIN)
+@sync_after(SkuSyncer)
+def patch_products_id_values_id(product_id: int, value_id: int) -> Response:
+    media_id, has_media_id = json_get("media_id", int)
+    order, has_order = json_get("order", int)
+    unit_price, has_unit_price = json_get("unit_price", int | float)
+
+    with conn.begin() as s:
+        # Get product value
+        product_value = s.query(ProductValue).filter_by(id=value_id).first()
+        if not product_value:
+            return json_response(404, ApiText.HTTP_404)
+
+        # Update product value
+        if has_media_id:
+            product_value.media_id = media_id
+        if has_unit_price:
+            product_value.unit_price = unit_price
+        if has_order:
+            product_value.order = order
+
+    return json_response()
+
+
+@api_v1_bp.delete("/products/<int:product_id>/values/<int:value_id>")
+@authorize(UserRoleLevel.ADMIN)
+def delete_products_id_values_id(product_id: int, value_id: int) -> Response:
+    with conn.begin() as s:
+        # Delete product value
+        product_value = s.query(ProductValue).filter_by(id=value_id).first()
+        if not product_value:
+            return json_response(404, ApiText.HTTP_404)
+        product_value.is_deleted = True
+
+        # Delete skus
+        skus = (
+            s.query(Sku)
+            .join(Sku.details)
+            .options(contains_eager(Sku.details))
+            .filter(SkuDetail.value_id == value_id, Sku.is_deleted == false())
+            .all()
+        )
+        for sku in skus:
+            sku.is_deleted = True
+
+    return json_response()
+
+
+#
+# Functions
+#
---
+++ web/blueprint/api_v1/routes/verification.py
@@ -0,0 +1,59 @@
+from enum import StrEnum
+
+from flask import abort
+from sqlalchemy.orm.session import Session
+from werkzeug import Response
+
+from web.api import API, json_response
+from web.blueprint.api_v1 import api_v1_bp
+from web.database import conn
+from web.database.model import Verification
+from web.i18n import _
+
+#
+# Configuration
+#
+
+
+class Text(StrEnum):
+    VERIFICATION_INVALID = _("API_VERIFICATION_INVALID")
+
+
+class VerificationAPI(API):
+    model = Verification
+    get_filters = {
+        Verification.key,
+    }
+    get_columns = {
+        Verification.id,
+        Verification.key,
+        Verification.user_id,
+    }
+
+
+#
+# Endpoints
+#
+
+
+@api_v1_bp.get("/verifications")
+def get_verifications() -> Response:
+    api = VerificationAPI()
+    data = api.gen_query_data(api.get_filters)
+    with conn.begin() as s:
+        filters = api.gen_query_filters(data, required=True)
+        models: list[Verification] = api.list_(s, *filters, limit=1)
+        for model in models:
+            val_verification(s, data, model)
+        resources = api.gen_resources(s, models)
+    return json_response(data=resources)
+
+
+#
+# Functions
+#
+
+
+def val_verification(s: Session, data: dict, model: Verification) -> None:
+    if not model.is_valid:
+        abort(json_response(400, Text.VERIFICATION_INVALID))
