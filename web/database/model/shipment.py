b0bdd598a3a4802efe693d95286fc8adc4537d3f
Stan Mertens <stnmertens@gmail.com>
2024-11-06 13:59:08 +0100

    remove shipment url and order_id constraint

--- web/database/model/shipment.py
+++ web/database/model/shipment.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, String, UniqueConstraint
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import mapped_column as MC
 from sqlalchemy.orm import relationship
 
@@ -7,7 +7,6 @@ from ._base import Attribute, Base
 
 class Shipment(Base, Attribute):
     __tablename__ = "shipment"
-    __table_args__ = (UniqueConstraint("url", "order_id"),)
 
     carrier = MC(String(64))
     code = MC(String(64))
