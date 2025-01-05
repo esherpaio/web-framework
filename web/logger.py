f9e718796064d26700c33cd82838cc8e1cf039c7
Stan Mertens <stnmertens@gmail.com>
2025-01-05 09:45:27 +0100

    html logger

--- web/logger.py
+++ web/logger.py
@@ -9,14 +9,6 @@ from web.config import config
 #
 
 
-class AnsiFormatter(logging.Formatter):
-    def format(self, record: logging.LogRecord) -> str:
-        color = _AnsiCode[record.levelname]
-        string = f"{color}%(levelname)s | %(message)s{_AnsiCode.RESET}"
-        formatter = logging.Formatter(string)
-        return formatter.format(record)
-
-
 class _AnsiCode(StrEnum):
     DEBUG = "\x1b[37m"
     INFO = "\x1b[37m"
@@ -28,9 +20,21 @@ class _AnsiCode(StrEnum):
 
 class PlainFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
-        string = "%(levelname)s | %(message)s"
-        formatter = logging.Formatter(string)
+        color = _AnsiCode[record.levelname]
+        template = f"{color}%(levelname)s | %(message)s{_AnsiCode.RESET}"
+        formatter = logging.Formatter(template)
         return formatter.format(record)
+
+
+class HtmlFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        template = "%(levelname)s | %(message)s"
+        formatter = logging.Formatter(template)
+        message = formatter.formatMessage(record)
+        if record.exc_info:
+            exception = self.formatException(record.exc_info)
+            message += f"<br><pre>{exception}</pre>"
+        return message
 
 
 #
@@ -42,7 +46,7 @@ class MailHandler(Handler):
     def __init__(self) -> None:
         super().__init__()
         self.setLevel(logging.ERROR)
-        self.setFormatter(PlainFormatter())
+        self.setFormatter(HtmlFormatter())
 
     def emit(self, record: LogRecord) -> None:
         from web.mail import send_email
@@ -68,7 +72,7 @@ def _get_logger(name: str) -> logging.Logger:
     base = logging.getLogger(name)
     base.setLevel(config.APP_LOG_LEVEL)
     stream = logging.StreamHandler()
-    stream.setFormatter(AnsiFormatter())
+    stream.setFormatter(PlainFormatter())
     base.addHandler(stream)
     if not config.APP_DEBUG and config.EMAIL_METHOD == "SMTP" and config.EMAIL_ADMIN:
         mail = MailHandler()
