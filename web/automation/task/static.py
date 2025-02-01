23abe68c39cd4574260903ce7ff9e2b6aa4ac63f
Stan Mertens <stnmertens@gmail.com>
2025-02-01 13:46:57 +0700

    update static syncer

--- web/automation/task/static.py
+++ web/automation/task/static.py
@@ -34,6 +34,10 @@ class StaticSeed:
         self.model: Type[AppSetting | AppBlueprint | AppRoute] = model
         self.endpoint: str = endpoint
 
+    @property
+    def id_(self) -> str:
+        return f"{self.model.__tablename__}:{self.type}"
+
     def get_resource(
         self,
         s: Session,
@@ -53,14 +57,12 @@ class StaticSeed:
         resource: AppSetting | AppBlueprint | AppRoute,
         cdn_path: str,
     ) -> None:
-        if self.type == StaticType.JS:
-            if resource.js_path != cdn_path:
-                resource.js_path = cdn_path
-                s.flush()
-        elif self.type == StaticType.CSS:
-            if resource.css_path != cdn_path:
-                resource.css_path = cdn_path
-                s.flush()
+        if self.type == StaticType.JS and resource.js_path != cdn_path:
+            resource.js_path = cdn_path
+            s.flush()
+        elif self.type == StaticType.CSS and resource.css_path != cdn_path:
+            resource.css_path = cdn_path
+            s.flush()
         else:
             log.error(f"Static seed type {self.type} unsupported")
 
@@ -87,9 +89,7 @@ class StaticSyncer(Syncer):
             for seed in cls.SEEDS:
                 resource = seed.get_resource(s)
                 if resource is None:
-                    log.error(
-                        f"Static resource {seed.type}:{seed.endpoint} is not found"
-                    )
+                    log.error(f"Static resource {seed.id_} is not found")
                     continue
                 resources[seed] = resource
         return resources
@@ -107,12 +107,12 @@ class StaticSyncer(Syncer):
             out_ext = packer.validate(seed.bundles)
             compiled, bytes_, hash_ = packer.pack(seed.bundles)
             if not compiled:
-                log.error(f"Static resource {seed.type}:{seed.endpoint} empty result")
+                log.error(f"Static resource {seed.id_} empty result")
                 continue
             present[(resource.__tablename__, resource.id)].append(seed.type)
             cdn_filename = f"{hash_}{out_ext}"
             if cdn_filename in cdn_filenames:
-                log.info(f"Static resource {seed.type}:{seed.endpoint} already exists")
+                log.info(f"Static resource {seed.id_} already exists")
                 continue
             cdn_path = os.path.join("static", cdn_filename)
             packer.write_cdn(bytes_, cdn_path)
