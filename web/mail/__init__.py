bd35a6b6a8f31a84fa6da62e0141601941de811a
Stan Mertens <stnmertens@gmail.com>
2025-03-08 22:51:35 +0100

    remove mail error

--- web/mail/__init__.py
+++ web/mail/__init__.py
@@ -1,9 +1,8 @@
 from .base import render_email, send_email
-from .mail import MailError, MailEvent, mail
+from .mail import MailEvent, mail
 
 __all__ = [
     "mail",
-    "MailError",
     "MailEvent",
     "render_email",
     "send_email",
