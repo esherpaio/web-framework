51aafd61c0b6581a6cff21fed2672370971b7d2d
Stan Mertens <stnmertens@gmail.com>
2025-03-10 13:39:22 +0100

    update web and worker;

--- web/web.py
+++ web/web.py
@@ -17,29 +17,8 @@ from web.database import conn
 from web.i18n import translator
 from web.locale import LocaleManager, current_locale
 from web.logger import log
-from web.mail import MailEvent, event, mail
+from web.mail import MailEvent, mail
 from web.optimizer import optimizer
-
-#
-# Defaults
-#
-
-
-MAIL_EVENTS: dict[MailEvent | str, list[Callable]] = {
-    MailEvent.ORDER_PAID: [event.mail_order_paid],
-    MailEvent.ORDER_RECEIVED: [event.mail_order_received],
-    MailEvent.ORDER_REFUNDED: [event.mail_order_refunded],
-    MailEvent.ORDER_SHIPPED: [event.mail_order_shipped],
-    MailEvent.USER_REQUEST_PASSWORD: [event.mail_user_password],
-    MailEvent.USER_REQUEST_VERIFICATION: [event.mail_user_verification],
-    MailEvent.WEBSITE_CONTACT: [event.mail_contact_business],
-    MailEvent.WEBSITE_BULK: [event.mail_bulk],
-}
-
-
-#
-# Classes
-#
 
 
 class Web:
@@ -158,7 +137,6 @@ class Web:
         else:
             log.warning("No email method configured")
 
-        mail.events.update(MAIL_EVENTS)
         if events:
             log.info(f"Updating {len(events)} mail events")
             mail.events.update(events)
--- web/worker.py
+++ web/worker.py
@@ -1,47 +1,72 @@
 import sched
 import time
-from typing import Type
+from typing import Callable, Type
 
 from web.automation import Automator
 from web.config import config
 from web.database import conn
+from web.i18n import translator
 from web.logger import log
+from web.mail import MailEvent, mail
 
 
 class Worker:
-    TASKS: list[Type[Automator]] = []
-
-    def __init__(self, interval: int = 300) -> None:
+    def __init__(
+        self,
+        mail_events: dict[MailEvent | str, list[Callable]] | None = None,
+        translations_dir: str | None = None,
+        automation_tasks: list[Type[Automator]] | None = None,
+    ) -> None:
         log.info("Initializing worker")
+
+        self.setup_i18n(translations_dir)
+        self.setup_mail(mail_events)
+
         self.scheduler = sched.scheduler(time.time, time.sleep)
-        interval = config.WORKER_INTERVAL_S
-        if not isinstance(interval, int):
-            raise ValueError(f"Invalid interval {interval}")
-        elif not 30 <= interval <= 3600:
+        interval_s = config.WORKER_INTERVAL_S
+        if not isinstance(interval_s, int):
+            raise ValueError(f"Invalid interval {interval_s}")
+        elif not 30 <= interval_s <= 3600:
             raise ValueError("Invalid must be between 30 and 3600 seconds")
-        self.interval = interval
-        self.set_tasks()
+        self.interval_s = interval_s
+        if automation_tasks is None:
+            automation_tasks = []
+        self.tasks = automation_tasks
 
-    def set_tasks(self) -> None:
-        from web.automation.task import EmailWorker
+    #
+    # Setup
+    #
 
-        self.TASKS = [EmailWorker]
+    def setup_i18n(self, dir_: str | None) -> None:
+        if dir_ is None:
+            return
+        log.info(f"Loading translations from {dir_}")
+        translator.load_dir(dir_)
 
-    def job(self) -> None:
-        for task in self.TASKS:
+    def setup_mail(self, events: dict[MailEvent | str, list[Callable]] | None) -> None:
+        if config.EMAIL_METHOD:
+            log.info(f"Configuring email method {config.EMAIL_METHOD}")
+        else:
+            log.warning("No email method configured")
+
+        if events:
+            log.info(f"Updating {len(events)} mail events")
+            mail.events.update(events)
+
+    #
+    # Loop
+    #
+
+    def _run(self) -> None:
+        for task in self.tasks:
             try:
                 log.info(f"Running task {task.__name__}")
                 with conn.begin() as s:
                     task.run(s)
             except Exception as e:
                 log.error(f"Error running task {task.__name__}: {e}")
-        self.scheduler.enter(self.interval, 1, self.job)
+        self.scheduler.enter(self.interval_s, 1, self._run)
 
     def start(self) -> None:
-        self.scheduler.enter(0, 1, self.job)
+        self.scheduler.enter(0, 1, self._run)
         self.scheduler.run()
-
-
-if __name__ == "__main__":
-    worker = Worker()
-    worker.start()
