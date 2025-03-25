fee2279a92a12c16b3870eeca9f3c8669a10d669
Stan Mertens <stnmertens@gmail.com>
2025-03-25 12:01:43 +0100

    gtags login and signup bug fix

--- web/app/blueprint/auth_v1/routes/login.py
+++ web/app/blueprint/auth_v1/routes/login.py
@@ -1,11 +1,11 @@
 from flask import render_template
 
 from web.app.blueprint.auth_v1 import auth_v1_bp
-from web.app.gtag import EventByClass, EventTrigger, GtagLogin
+from web.app.gtag import EventByClass, EventTrigger, GtagLogin, Gtags
 
 
 @auth_v1_bp.get("/login")
 @auth_v1_bp.get("/<string:_locale>/login")
 def login(_locale: str) -> str:
-    gtags = [GtagLogin(EventByClass(EventTrigger.ON_SUBMIT))]
+    gtags = Gtags(GtagLogin(EventByClass(EventTrigger.ON_SUBMIT)))
     return render_template("auth/login.html", gtags=gtags)
