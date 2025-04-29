5452cd0a2bacd8fbac96ab17f252fcf0817f03aa
Stan Mertens <stnmertens@gmail.com>
2025-04-29 21:50:24 +0200

    add modal js

--- web/app/javascript/modals.js
+++ web/app/javascript/modals.js
@@ -1,9 +1,21 @@
-function showMessage(message) {
+function hideModals() {
     const elements = document.getElementsByClassName("modal");
     for (const element of elements) {
         const modal = bootstrap.Modal.getInstance(element);
-        if (modal) modal.hide();
+        if (modal) {
+            modal.hide();
+        }
     }
+}
+
+function showModal(id_) {
+    hideModals();
+    const modal = new bootstrap.Modal(document.getElementById(id_));
+    modal.show();
+}
+
+function showMessage(message) {
+    hideModals();
     let modalBody = document.querySelector("#modal-message .modal-body p");
     modalBody.innerHTML = message;
     let modal = new bootstrap.Modal(document.getElementById("modal-message"));
