5a288640b4cef359ba9252ea9beceaf68bc4cb36
Stan Mertens <stnmertens@gmail.com>
2025-07-05 11:19:25 +0200

    try out config validation

--- config.py
+++ config.py
@@ -1,112 +1,115 @@
 import os
 from decimal import Decimal
 
+from web.config import get_env_var
+
 # Application
 # NOTE: keep the secret key used in production secret
 # NOTE: do not run with debug turned on in production
 
-APP_ROOT: str = os.path.dirname(os.path.abspath(__file__))
-APP_LOG_LEVEL: str = "INFO"
-APP_DEBUG: bool = True
-APP_DEBUG_PORT: int = 5000
-APP_URL_SCHEME: str = "http"
-APP_OPTIMIZE: bool = False
-APP_SYNC_EXTERNAL: bool = True
-APP_SYNC_STATIC: bool = True
-APP_SYNC_TIMEOUT_S: int = 10
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_LOG_LEVEL = get_env_var("APP_LOG_LEVEL", str, default="INFO")
+APP_DEBUG = get_env_var("APP_DEBUG", bool, default=False)
+APP_DEBUG_PORT = get_env_var("APP_DEBUG_PORT", int, default=5000)
+APP_URL_SCHEME = get_env_var("APP_URL_SCHEME", str, default="https")
+APP_OPTIMIZE = get_env_var("APP_OPTIMIZE", bool, default=False)
+APP_SYNC_EXTERNAL = get_env_var("APP_SYNC_EXTERNAL", bool, default=True)
+APP_SYNC_STATIC = get_env_var("APP_SYNC_STATIC", bool, default=True)
+APP_SYNC_TIMEOUT_S = get_env_var("APP_SYNC_TIMEOUT_S", int, default=10)
 
-WORKER_ENABLED: bool = False
-WORKER_INTERVAL_S: int = 300
+WORKER_ENABLED = get_env_var("WORKER_ENABLED", bool, default=False)
+WORKER_INTERVAL_S = get_env_var("WORKER_INTERVAL_S", int, default=300)
 
 # Authentication
 
-AUTH_JWT_COOKIE_NAME: str = "access_token"
-AUTH_JWT_ENCODE_ALGORITHM: str = "HS256"
-AUTH_JWT_DECODE_ALGORITHMS: list[str] = ["HS256"]
-AUTH_JWT_SECRET: str = "secret"
-AUTH_JWT_ALLOW_GUEST: bool = False
-AUTH_JWT_EXPIRES_S: int = 86400
-AUTH_JWT_DECODE_LEEWAY_S: int = 0
+AUTH_JWT_COOKIE_NAME = "access_token"
+AUTH_JWT_ENCODE_ALGORITHM = "HS256"
+AUTH_JWT_DECODE_ALGORITHMS = ["HS256"]
+AUTH_JWT_SECRET = get_env_var("AUTH_JWT_SECRET", str)
+AUTH_JWT_ALLOW_GUEST = get_env_var("AUTH_JWT_ALLOW_GUEST", bool, default=False)
+AUTH_JWT_EXPIRES_S = get_env_var("AUTH_JWT_EXPIRES_S", int, default=3600)
+AUTH_JWT_DECODE_LEEWAY_S = get_env_var("AUTH_JWT_DECODE_LEEWAY_S", int, default=60)
 
-AUTH_KEY_HEADER_NAME: str = "Authorization"
+AUTH_KEY_HEADER_NAME = "Authorization"
 
-AUTH_CSRF_COOKIE_NAME: str = "csrf_token"
-AUTH_CSRF_METHODS: list[str] = ["POST", "PUT", "PATCH", "DELETE"]
+AUTH_CSRF_COOKIE_NAME = "csrf_token"
+AUTH_CSRF_METHODS = ["POST", "PUT", "PATCH", "DELETE"]
 
 # Endpoints
 
-ENDPOINT_HOME: None | str = None
-ENDPOINT_ERROR: None | str = None
-ENDPOINT_LOGIN: None | str = None
-ENDPOINT_PASSWORD_RECOVERY: None | str = None
+ENDPOINT_HOME = None
+ENDPOINT_ERROR = None
+ENDPOINT_LOGIN = None
+ENDPOINT_PASSWORD_RECOVERY = None
 
 # Integrations
 
-DATABASE_URL: None | str = None
-LOCALHOST_URL: None | str = None
+DATABASE_URL = get_env_var("DATABASE_URL", str)
+LOCALHOST_URL = get_env_var("LOCALHOST_URL", str)
 
-MOLLIE_KEY: None | str = None
+MOLLIE_KEY = get_env_var("MOLLIE_KEY", str)
 
-GOOGLE_KEY: None | str = None
-GOOGLE_CLIENT_ID: None | str = None
-GOOGLE_PLACE_ID: None | str = None
+GOOGLE_KEY = get_env_var("GOOGLE_KEY", str)
+GOOGLE_CLIENT_ID = get_env_var("GOOGLE_CLIENT_ID", str)
+GOOGLE_PLACE_ID = get_env_var("GOOGLE_PLACE_ID", str)
 
-INTIME_INTEGRATION: bool = False
+INTIME_INTEGRATION = False
 
 # Content delivery network
 
-CDN_URL: None | str = None
-CDN_HOSTNAME: None | str = None
-CDN_USERNAME: None | str = None
-CDN_PASSWORD: None | str = None
+CDN_ENABLED = False
+CDN_URL = None
+CDN_HOSTNAME = get_env_var("CDN_HOSTNAME", str)
+CDN_USERNAME = get_env_var("CDN_USERNAME", str)
+CDN_PASSWORD = get_env_var("CDN_PASSWORD", str)
 
-CDN_AUTO_NAMING: bool = False
-CDN_IMAGE_EXTS: list[str] = ["jpg", "jpeg", "png", "webp"]
-CDN_AUDIO_EXTS: list[str] = ["m4a", "mp3", "mp4"]
-CDN_VIDEO_EXTS: list[str] = ["mp4"]
+CDN_AUTO_NAMING = False
+CDN_IMAGE_EXTS = ["jpg", "jpeg", "png", "webp"]
+CDN_AUDIO_EXTS = ["m4a", "mp3", "mp4"]
+CDN_VIDEO_EXTS = ["mp4"]
 
 # Email configuration
 
-EMAIL_METHOD: str = "SMTP"
-EMAIL_TIMEOUT_S: int = 10
-EMAIL_MAX_RECIPIENTS: int = 100
-EMAIL_FROM: None | str = None
-EMAIL_TO: None | str = None
-EMAIL_ADMIN: None | str = None
+EMAIL_METHOD = "SMTP"
+EMAIL_TIMEOUT_S = get_env_var("EMAIL_TIMEOUT_S", int, default=10)
+EMAIL_MAX_RECIPIENTS = 100
+EMAIL_FROM = get_env_var("EMAIL_FROM", str)
+EMAIL_TO = get_env_var("EMAIL_TO", str)
+EMAIL_ADMIN = get_env_var("EMAIL_ADMIN", str)
 
-SMTP_HOSTNAME: None | str = None
-SMTP_PORT: int = 587
-SMTP_USERNAME: None | str = None
-SMTP_PASSWORD: None | str = None
+SMTP_HOSTNAME = get_env_var("SMTP_HOSTNAME", str)
+SMTP_PORT = 587
+SMTP_USERNAME = get_env_var("SMTP_USERNAME", str)
+SMTP_PASSWORD = get_env_var("SMTP_PASSWORD", str)
 
 # Website details
 
-WEBSITE_NAME: None | str = None
-WEBSITE_COUNTRY_CODE: None | str = None
-WEBSITE_LANGUAGE_CODE: None | str = None
-WEBSITE_FAVICON_URL: None | str = None
-WEBSITE_LOGO_URL: None | str = None
-WEBSITE_HEX_COLOR: None | str = None
+WEBSITE_NAME = None
+WEBSITE_COUNTRY_CODE = None
+WEBSITE_LANGUAGE_CODE = None
+WEBSITE_FAVICON_URL = None
+WEBSITE_LOGO_URL = None
+WEBSITE_HEX_COLOR = None
 
 # Business details
 
-BUSINESS_NAME: None | str = None
-BUSINESS_EMAIL: None | str = None
-BUSINESS_CC: None | str = None
-BUSINESS_VAT: None | str = None
-BUSINESS_VAT_RATE: Decimal = Decimal("1")
-BUSINESS_VAT_REVERSE_CHARGE: bool = False
-BUSINESS_STREET: None | str = None
-BUSINESS_CITY: None | str = None
-BUSINESS_ZIP_CODE: None | str = None
-BUSINESS_COUNTRY: None | str = None
-BUSINESS_COUNTRY_CODE: None | str = None
+BUSINESS_NAME = None
+BUSINESS_EMAIL = None
+BUSINESS_CC = None
+BUSINESS_VAT = None
+BUSINESS_VAT_RATE = Decimal("1")
+BUSINESS_VAT_REVERSE_CHARGE = False
+BUSINESS_STREET = None
+BUSINESS_CITY = None
+BUSINESS_ZIP_CODE = None
+BUSINESS_COUNTRY = None
+BUSINESS_COUNTRY_CODE = None
 
 # Social media links
 
-SOCIAL_DISCORD: None | str = None
-SOCIAL_FACEBOOK: None | str = None
-SOCIAL_INSTAGRAM: None | str = None
-SOCIAL_PINTEREST: None | str = None
-SOCIAL_TWITTER: None | str = None
-SOCIAL_YOUTUBE: None | str = None
+SOCIAL_DISCORD = None
+SOCIAL_FACEBOOK = None
+SOCIAL_INSTAGRAM = None
+SOCIAL_PINTEREST = None
+SOCIAL_TWITTER = None
+SOCIAL_YOUTUBE = None
--- web/config.py
+++ web/config.py
@@ -1,19 +1,120 @@
 import importlib
 import os
-from typing import Any
+from decimal import Decimal
+from typing import Any, Protocol, cast
+
+from web.error import ConfigError
+
+
+class ConfigProtocol(Protocol):
+    APP_ROOT: str
+    APP_LOG_LEVEL: str
+    APP_DEBUG: bool
+    APP_DEBUG_PORT: int
+    APP_URL_SCHEME: str
+    APP_OPTIMIZE: bool
+    APP_SYNC_EXTERNAL: bool
+    APP_SYNC_STATIC: bool
+    APP_SYNC_TIMEOUT_S: int
+    WORKER_ENABLED: bool
+    WORKER_INTERVAL_S: int
+
+    AUTH_JWT_COOKIE_NAME: str
+    AUTH_JWT_ENCODE_ALGORITHM: str
+    AUTH_JWT_DECODE_ALGORITHMS: list[str]
+    AUTH_JWT_SECRET: str
+    AUTH_JWT_ALLOW_GUEST: bool
+    AUTH_JWT_EXPIRES_S: int
+    AUTH_JWT_DECODE_LEEWAY_S: int
+    AUTH_KEY_HEADER_NAME: str
+    AUTH_CSRF_COOKIE_NAME: str
+    AUTH_CSRF_METHODS: list[str]
+
+    ENDPOINT_HOME: str | None
+    ENDPOINT_ERROR: str | None
+    ENDPOINT_LOGIN: str | None
+    ENDPOINT_PASSWORD_RECOVERY: str | None
+
+    DATABASE_URL: str
+    LOCALHOST_URL: str | None
+    MOLLIE_KEY: str | None
+    GOOGLE_KEY: str | None
+    GOOGLE_CLIENT_ID: str | None
+    GOOGLE_PLACE_ID: str | None
+    INTIME_INTEGRATION: bool
+
+    CDN_ENABLED: bool
+    CDN_URL: str | None
+    CDN_HOSTNAME: str | None
+    CDN_USERNAME: str | None
+    CDN_PASSWORD: str | None
+    CDN_AUTO_NAMING: bool
+    CDN_IMAGE_EXTS: list[str]
+    CDN_AUDIO_EXTS: list[str]
+    CDN_VIDEO_EXTS: list[str]
+
+    EMAIL_METHOD: str
+    EMAIL_TIMEOUT_S: int
+    EMAIL_MAX_RECIPIENTS: int
+    EMAIL_FROM: str | None
+    EMAIL_TO: str | None
+    EMAIL_ADMIN: str | None
+    SMTP_HOSTNAME: str | None
+    SMTP_PORT: int
+    SMTP_USERNAME: str | None
+    SMTP_PASSWORD: str | None
+
+    WEBSITE_NAME: str | None
+    WEBSITE_COUNTRY_CODE: str | None
+    WEBSITE_LANGUAGE_CODE: str | None
+    WEBSITE_FAVICON_URL: str | None
+    WEBSITE_LOGO_URL: str | None
+    WEBSITE_HEX_COLOR: str | None
+
+    BUSINESS_NAME: str | None
+    BUSINESS_EMAIL: str | None
+    BUSINESS_CC: str | None
+    BUSINESS_VAT: str | None
+    BUSINESS_VAT_RATE: Decimal
+    BUSINESS_VAT_REVERSE_CHARGE: bool
+    BUSINESS_STREET: str | None
+    BUSINESS_CITY: str | None
+    BUSINESS_ZIP_CODE: str | None
+    BUSINESS_COUNTRY: str | None
+    BUSINESS_COUNTRY_CODE: str | None
+
+    SOCIAL_DISCORD: str | None
+    SOCIAL_FACEBOOK: str | None
+    SOCIAL_INSTAGRAM: str | None
+    SOCIAL_PINTEREST: str | None
+    SOCIAL_TWITTER: str | None
+    SOCIAL_YOUTUBE: str | None
 
 
 class Config:
-    _wrapped = None
+    _config: ConfigProtocol | None = None
 
     def __getattr__(self, name: str) -> Any:
-        if self._wrapped is None:
+        if self._config is None:
             self._setup()
-        return getattr(self._wrapped, name)
+        return getattr(self._config, name)
 
     def _setup(self) -> None:
         module_path = os.environ["APP_CONFIG_MODULE"]
-        self._wrapped = importlib.import_module(module_path)
+        config = cast(ConfigProtocol, importlib.import_module(module_path))
+        self._validate(config)
+        return config
+
+    def _validate(self, config: ConfigProtocol) -> None:
+        if config.CDN_ENABLED:
+            if not config.CDN_URL:
+                raise ConfigError("CDN_URL")
+            if not config.CDN_HOSTNAME:
+                raise ConfigError("CDN_HOSTNAME")
+            if not config.CDN_USERNAME:
+                raise ConfigError("CDN_USERNAME")
+            if not config.CDN_PASSWORD:
+                raise ConfigError("CDN_PASSWORD")
 
 
 def get_env_var(key: str, type_: Any, default: Any = None) -> Any:
@@ -26,4 +127,4 @@ def get_env_var(key: str, type_: Any, default: Any = None) -> Any:
         return value in ["true", "1"]
 
 
-config = Config()
+config: ConfigProtocol = Config()
