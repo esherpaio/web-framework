fab71022232851443e48291054ff16b0a4c9d7e7
Stan Mertens <stnmertens@gmail.com>
2025-07-08 14:43:17 +0200

    update config module env var

--- web/setup/environment.py
+++ web/setup/environment.py
@@ -121,7 +121,7 @@ class Config:
         return getattr(self._config, name)
 
     def _setup(self) -> None:
-        module_path = os.getenv("SUMMIT_CONFIG_MODULE", "config")
+        module_path = os.getenv("CONFIG_MODULE", "config")
         config_ = importlib.import_module(module_path)
         self._validate(config_)
         self._config = config_
