cf4ff298ff81bb08c0177d60c04eecb7087c7609
Stan Mertens <stnmertens@gmail.com>
2026-06-03 10:08:14 +0700

    do not send emails in debug mode

--- web/automation/task/email.py
+++ web/automation/task/email.py
@@ -4,8 +4,9 @@ from sqlalchemy import null, or_
 from sqlalchemy.orm import Session
 
 from web.database import conn
-from web.database.model import Email, EmailStatusId
+from web.database.model import Email, EmailStatusId, User, UserRoleId
 from web.mail import mail
+from web.setup import config
 
 from ..automator import Processor
 
@@ -21,21 +22,19 @@ class EmailProcessor(Processor):
 
     @staticmethod
     def send_email(s: Session, max_weeks: int = 4) -> bool:
-        email = (
-            s.query(Email)
-            .filter(
-                Email.created_at
-                > datetime.now(timezone.utc) - timedelta(weeks=max_weeks),
-                or_(
-                    Email.updated_at == null(),
-                    Email.updated_at < datetime.now(timezone.utc) - timedelta(days=1),
-                ),
-                Email.status_id.in_([EmailStatusId.QUEUED, EmailStatusId.FAILED]),
-            )
-            .order_by(Email.id.asc())
-            .with_for_update(skip_locked=True)
-            .first()
+        query = s.query(Email).filter(
+            Email.created_at > datetime.now(timezone.utc) - timedelta(weeks=max_weeks),
+            or_(
+                Email.updated_at == null(),
+                Email.updated_at < datetime.now(timezone.utc) - timedelta(days=1),
+            ),
+            Email.status_id.in_([EmailStatusId.QUEUED, EmailStatusId.FAILED]),
         )
+        if config.DEBUG:
+            query = query.join(User, User.id == Email.user_id).filter(
+                User.role_id == UserRoleId.ADMIN
+            )
+        email = query.order_by(Email.id.asc()).with_for_update(skip_locked=True).first()
         if email is None:
             return False
         mail.trigger_events(s, email.event_id, _email=email, **email.data)
